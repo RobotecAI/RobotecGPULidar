0fedffe91c949baf7fd178fa84e3d1be88ad1f3d
Adam Dabrowski <adam.dabrowski@robotec.ai>
2021-02-18 17:22:01 +0100

    corrections and expanded test

--- dotnet_plugin/gpu-lidar-raycaster-dotnet-test/src/SimpleModelTest.cs
+++ dotnet_plugin/gpu-lidar-raycaster-dotnet-test/src/SimpleModelTest.cs
@@ -20,15 +20,31 @@ namespace ConsoleApplication
                                           new Vector3f() { x = 0, y = 1, z = 0},
                                           new Vector3f() { x = 0, y = 0, z = 1}
                                         },
-              texture_coordinates = new Vector2f[2],
+              texture_coordinates = new Vector2f[3],
               indices = new Vector3i[3] { new Vector3i() { x = 0, y = 0, z = 0},
                                           new Vector3i() { x = 1, y = 0, z = 0},
                                           new Vector3i() { x = 2, y = 0, z = 0}
                                         }
             };
 
-            //Console.WriteLine("Vertices ", m.vertices.Length);
+            Console.WriteLine("Vertices " + m.vertices.Length);
             lidar.AddModel(m);
+            lidar.UpdateModel(m);
+
+            LidarSource ls = new LidarSource()
+            {
+              source_id = "test_lidar",
+              source_pos = new Point3f() { x = 0, y = 0, z = 0 },
+              directions = new Point3f[3] { new Point3f() { x = 1, y = 0, z = 0 },
+                                            new Point3f() { x = 0, y = 1, z = 0 },
+                                            new Point3f() { x = 0, y = 0, z = 1 }
+                                          },
+              range = 10
+            };
+
+            RaycastResults rr = new RaycastResults();
+            lidar.Raycast(ls, ref rr);
+            Console.WriteLine("Results  " + rr.points.Length);
             lidar.Dispose();
         }
     }
