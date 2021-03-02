3cac8b156e966680ecc60178385f39a9b891f23d
Adam Dabrowski <adam.dabrowski@robotec.ai>
2021-03-02 12:12:37 +0100

    useful scripts

---
+++ run.sh
@@ -0,0 +1,1 @@
+build/dotnet_plugin/gpu-lidar-raycaster-dotnet-test/Release/netcoreapp2.0/linux-x64/SimpleModelTest
---
+++ sync.sh
@@ -0,0 +1,3 @@
+cp build/*.so build/dotnet_plugin/gpu-lidar-raycaster-dotnet-test/Release/netcoreapp2.0/linux-x64/
+cp build/*.so ../robotec-simulator/src/robotec-simulator/Robotec_Simulator/Assets/Plugins/x86_64
+cp build/dotnet_plugin/gpu-lidar-raycaster-dotnet/Release/netcoreapp2.0/linux-x64/publish/*.dll ../robotec-simulator/src/robotec-simulator/Robotec_Simulator/Assets/Plugins/
