53f3afa54194eafea4df1f42d4c44ae752b00298
Adam Dabrowski <adam.dabrowski@robotec.ai>
2021-03-03 11:49:00 +0100

    updated scripts and added .gitignore

--- sync.sh
+++ sync.sh
@@ -1,3 +1,2 @@
-cp build/*.so build/dotnet_plugin/gpu-lidar-raycaster-dotnet-test/Release/netcoreapp2.0/linux-x64/
-cp build/*.so ../robotec-simulator/src/robotec-simulator/Robotec_Simulator/Assets/Plugins/x86_64
-cp build/dotnet_plugin/gpu-lidar-raycaster-dotnet/Release/netcoreapp2.0/linux-x64/publish/*.dll ../robotec-simulator/src/robotec-simulator/Robotec_Simulator/Assets/Plugins/
+cp -v build/*.so ../robotec-simulator/src/robotec-simulator/Robotec_Simulator/Assets/Plugins/x86_64
+cp -v build/dotnet_plugin/gpu-lidar-raycaster-dotnet/Release/netcoreapp2.0/linux-x64/publish/*.dll ../robotec-simulator/src/robotec-simulator/Robotec_Simulator/Assets/Plugins/
