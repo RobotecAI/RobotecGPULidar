cfe3bc9b35a387573a4236abec4d8dda390b7038
Adam Dąbrowski <1961316-adamdbrw@users.noreply.gitlab.com>
2021-03-08 15:13:06 +0000

    Merge branch 'error_handling' into 'master'
    error handling
    
    See merge request robotec.ai/internal/simulation-platform/modules/lidar-raycasting-gpu!6

--- dotnet_plugin/gpu-lidar-raycaster-dotnet/src/Raycaster.cs
+++ dotnet_plugin/gpu-lidar-raycaster-dotnet/src/Raycaster.cs
@@ -8,11 +8,13 @@ namespace GPULidarRaycaster
 public class Raycaster : IDisposable
 {
   private IntPtr m_NativeRaycaster = IntPtr.Zero;
-  public Raycaster() {
+  public Raycaster()
+  {
     m_NativeRaycaster = NativeMethods.Internal_CreateNativeRaycaster();
   }
 
-  ~Raycaster() {
+  ~Raycaster()
+  {
     Dispose(false);
   }
 
@@ -23,33 +25,44 @@ public class Raycaster : IDisposable
 
   private void Dispose(bool disposing)
   {
-    if (!disposed)
-    {
-      if(disposing)
-      {
-          // Dispose managed resources.
+    if (!disposed) {
+      if(disposing) {
+        // Dispose managed resources.
       }
-
       Destroy();
       disposed = true;
     }
   }
 
-  public void AddOrUpdateMesh(Mesh mesh) {
-    NativeHandleCheck();
-    NativeMethods.Internal_AddOrUpdateMesh(m_NativeRaycaster, mesh.id, mesh.vertices, mesh.normals,
-      mesh.texture_coordinates, mesh.indices, mesh.indices.Length, mesh.vertices.Length);
+  private void CheckError(int native_error_code)
+  {
+    if (native_error_code == 0)
+      return;
+
+    IntPtr errorPtr = NativeMethods.Internal_GetLastError();
+    string error = Marshal.PtrToStringAnsi(errorPtr);
+    throw new Exception("Native raycaster exception: [" + error + "]");
   }
 
-  public void Raycast(LidarSource source, ref RaycastResults res) {
+  public void AddOrUpdateMesh(Mesh mesh)
+  {
     NativeHandleCheck();
-    NativeMethods.Internal_Raycast(m_NativeRaycaster, source.source_id, source.source_pos, source.directions,
-      source.directions.Length, source.range);
+    CheckError(NativeMethods.Internal_AddOrUpdateMesh(m_NativeRaycaster, mesh.id, mesh.vertices,
+      mesh.normals, mesh.texture_coordinates, mesh.indices, mesh.indices.Length, mesh.vertices.Length));
+  }
+
+  public void Raycast(LidarSource source, ref RaycastResults res)
+  {
+    NativeHandleCheck();
+    Console.WriteLine("Raycasting");
+    CheckError(NativeMethods.Internal_Raycast(m_NativeRaycaster, source.source_id,
+     source.source_pos, source.directions, source.directions.Length, source.range));
 
     // Get points right away - this could also be done in a different time
     IntPtr results_raw = new IntPtr();
     int results_count = 0;
-    NativeMethods.Internal_GetPoints(m_NativeRaycaster, ref results_raw, ref results_count);
+    Console.WriteLine("Getting points.");
+    CheckError(NativeMethods.Internal_GetPoints(m_NativeRaycaster, ref results_raw, ref results_count));
 
     res.lidar_id = source.source_id;
     res.points = new Point4f[results_count];
@@ -68,13 +81,15 @@ public class Raycaster : IDisposable
     }
   }
 
-  private void NativeHandleCheck() {
+  private void NativeHandleCheck()
+  {
     if (m_NativeRaycaster == IntPtr.Zero) {
       throw new Exception("Native raycaster object not created or destroyed already!");
     }
   }
 
-  private void Destroy() {
+  private void Destroy()
+  {
     if (m_NativeRaycaster != IntPtr.Zero) {
       NativeMethods.Internal_DestroyNativeRaycaster(m_NativeRaycaster);
       m_NativeRaycaster = IntPtr.Zero;
