96dc6784b74ca1a130572d6aecb4aed5ff167955
Piotr Rybicki <piotr.rybicki@robotec.ai>
2021-08-09 13:52:57 +0200

    Simplify ModelInstance implementation

--- include/ModelInstance.h
+++ include/ModelInstance.h
@@ -10,34 +10,17 @@
 
 class ModelInstance {
 public:
-    ModelInstance(std::shared_ptr<TriangleMesh> mesh);
+    ModelInstance(std::shared_ptr<TriangleMesh> mesh, OptixDeviceContext optixContext);
     ~ModelInstance();
-    OptixTraversableHandle buildGAS(OptixDeviceContext optixContext);
+
     OptixInstance buildIAS(unsigned int id);
-    void updateMesh(std::shared_ptr<TriangleMesh> mesh);
-
-    //(TODO) make a map since one model can contain multiple meshes
-    std::shared_ptr<TriangleMesh> m_triangle_mesh;
-    std::shared_ptr<Texture> m_texture;
-
-    bool needs_rebuild { false };
 
     CUDABuffer m_vertex_buffer;
     CUDABuffer m_normal_buffer;
     CUDABuffer m_texcoord_buffer;
     CUDABuffer m_index_buffer;
-
+    std::shared_ptr<TriangleMesh> m_triangle_mesh;
 private:
-    OptixTraversableHandle _GAS_handle;
-    OptixBuildInput triangleInput;
-    CUDABuffer outputBuffer;
-    CUDABuffer asBuffer;
-    OptixInstance instance;
+    CUDABuffer compactedGASBuffer;
     OptixTraversableHandle meshHandle;
-    CUdeviceptr d_vertices;
-    CUdeviceptr d_indices;
-    CUdeviceptr d_transforms;
-    uint32_t triangleInputFlags;
 };
-
-typedef std::unordered_map<std::string, std::shared_ptr<ModelInstance>> InstancesMap;
--- src/ModelInstance.cpp
+++ src/ModelInstance.cpp
@@ -8,7 +8,7 @@
 
 using namespace gdt;
 
-ModelInstance::ModelInstance(std::shared_ptr<TriangleMesh> mesh)
+ModelInstance::ModelInstance(std::shared_ptr<TriangleMesh> mesh, OptixDeviceContext optixContext)
     : m_triangle_mesh(mesh)
 {
     m_vertex_buffer.alloc_and_upload(mesh->vertex);
@@ -18,132 +18,92 @@ ModelInstance::ModelInstance(std::shared_ptr<TriangleMesh> mesh)
     if (!mesh->texcoord.empty())
         m_texcoord_buffer.alloc_and_upload(mesh->texcoord);
 
-    triangleInput = {};
-    triangleInput.type = OPTIX_BUILD_INPUT_TYPE_TRIANGLES;
+    CUdeviceptr d_vertices = m_vertex_buffer.d_pointer();
+    CUdeviceptr d_indices = m_index_buffer.d_pointer();
+    unsigned triangleInputFlags = OPTIX_GEOMETRY_FLAG_DISABLE_ANYHIT;
 
-    d_vertices = m_vertex_buffer.d_pointer();
-    d_indices = m_index_buffer.d_pointer();
+    OptixBuildInput triangleInput = {
+        .type = OPTIX_BUILD_INPUT_TYPE_TRIANGLES,
+        .triangleArray = {
+            .vertexBuffers = &d_vertices,
+            .numVertices = static_cast<unsigned>(mesh->vertex.size()),
+            .vertexFormat = OPTIX_VERTEX_FORMAT_FLOAT3,
+            .vertexStrideInBytes = sizeof(vec3f),
+            .indexBuffer = d_indices,
+            .numIndexTriplets = static_cast<unsigned>(mesh->index.size()),
+            .indexFormat = OPTIX_INDICES_FORMAT_UNSIGNED_INT3,
+            .indexStrideInBytes = sizeof(vec3i),
+            .flags = &triangleInputFlags,
+            .numSbtRecords = 1,
+            .sbtIndexOffsetBuffer = 0,
+            .sbtIndexOffsetSizeInBytes = 0,
+            .sbtIndexOffsetStrideInBytes = 0,
+        }
+    };
 
-    triangleInput.triangleArray.vertexFormat = OPTIX_VERTEX_FORMAT_FLOAT3;
-    triangleInput.triangleArray.vertexStrideInBytes = sizeof(vec3f);
-    triangleInput.triangleArray.numVertices = (int)mesh->vertex.size();
-    triangleInput.triangleArray.vertexBuffers = &d_vertices;
-
-    triangleInput.triangleArray.indexFormat = OPTIX_INDICES_FORMAT_UNSIGNED_INT3;
-    triangleInput.triangleArray.indexStrideInBytes = sizeof(vec3i);
-    triangleInput.triangleArray.numIndexTriplets = (int)mesh->index.size();
-    triangleInput.triangleArray.indexBuffer = d_indices;
-
-    triangleInputFlags = OPTIX_GEOMETRY_FLAG_DISABLE_ANYHIT;
-
-    triangleInput.triangleArray.flags = &triangleInputFlags;
-    triangleInput.triangleArray.numSbtRecords = 1;
-    triangleInput.triangleArray.sbtIndexOffsetBuffer = 0;
-    triangleInput.triangleArray.sbtIndexOffsetSizeInBytes = 0;
-    triangleInput.triangleArray.sbtIndexOffsetStrideInBytes = 0;
-}
-
-void ModelInstance::updateMesh(std::shared_ptr<TriangleMesh> mesh)
-{
-    m_triangle_mesh = mesh;
-    needs_rebuild = true;
-}
-
-OptixTraversableHandle ModelInstance::buildGAS(OptixDeviceContext optixContext)
-{
-    OptixAccelBuildOptions accelOptions = {};
-    accelOptions.buildFlags = OPTIX_BUILD_FLAG_PREFER_FAST_TRACE | OPTIX_BUILD_FLAG_ALLOW_UPDATE | OPTIX_BUILD_FLAG_ALLOW_COMPACTION;
-    accelOptions.motionOptions.numKeys = 0; // no motion blur
-
-    if (needs_rebuild)
-        accelOptions.operation = OPTIX_BUILD_OPERATION_UPDATE;
-    else
-        accelOptions.operation = OPTIX_BUILD_OPERATION_BUILD;
+    OptixAccelBuildOptions accelOptions = {
+        .buildFlags = OPTIX_BUILD_FLAG_PREFER_FAST_TRACE | OPTIX_BUILD_FLAG_ALLOW_UPDATE | OPTIX_BUILD_FLAG_ALLOW_COMPACTION,
+        .operation = OPTIX_BUILD_OPERATION_BUILD
+    };
 
     OptixAccelBufferSizes blasBufferSizes;
     OPTIX_CHECK(optixAccelComputeMemoryUsage(optixContext,
-        &accelOptions,
-        &triangleInput,
-        1, // num_build_inputs
-        &blasBufferSizes));
+                                             &accelOptions,
+                                             &triangleInput, 1,
+                                             &blasBufferSizes));
 
-    // ==================================================================
     // prepare compaction
-    // ==================================================================
-
-    CUDABuffer compactedSizeBuffer;
+    CUDABuffer compactedSizeBuffer, tempGASBuffer, fullGASBuffer;
+    tempGASBuffer.alloc(blasBufferSizes.tempSizeInBytes);
+    fullGASBuffer.alloc(blasBufferSizes.outputSizeInBytes);
     compactedSizeBuffer.alloc(sizeof(uint64_t));
 
-    OptixAccelEmitDesc emitDesc;
-    emitDesc.type = OPTIX_PROPERTY_TYPE_COMPACTED_SIZE;
-    emitDesc.result = compactedSizeBuffer.d_pointer();
+    OptixAccelEmitDesc emitDesc = {
+        .result = compactedSizeBuffer.d_pointer(),
+        .type = OPTIX_PROPERTY_TYPE_COMPACTED_SIZE,
+    };
 
-    // ==================================================================
     // execute build (main stage)
-    // ==================================================================
-
-    CUDABuffer tempBuffer;
-    tempBuffer.alloc(blasBufferSizes.tempSizeInBytes);
-
-    if (!needs_rebuild) {
-        // for update we use existing buffer
-        // in rebuild it should not change size
-        // if size changes, build from begining
-        outputBuffer.free();
-        outputBuffer.alloc(blasBufferSizes.outputSizeInBytes);
-    }
-
     OPTIX_CHECK(optixAccelBuild(optixContext,
-        /* stream */ 0,
-        &accelOptions,
-        &triangleInput,
-        1,
-        tempBuffer.d_pointer(),
-        tempBuffer.sizeInBytes,
-
-        outputBuffer.d_pointer(),
-        outputBuffer.sizeInBytes,
-
-        &meshHandle,
-
-        &emitDesc, 1));
+                                nullptr,
+                                &accelOptions,
+                                &triangleInput, 1,
+                                tempGASBuffer.d_pointer(),
+                                tempGASBuffer.sizeInBytes,
+                                fullGASBuffer.d_pointer(),
+                                fullGASBuffer.sizeInBytes,
+                                &meshHandle,
+                                &emitDesc, 1));
     CUDA_SYNC_CHECK();
 
     uint64_t compactedSize;
     compactedSizeBuffer.download(&compactedSize, 1);
 
-    asBuffer.free();
-    asBuffer.alloc(compactedSize);
+    compactedGASBuffer.alloc(compactedSize);
     OPTIX_CHECK(optixAccelCompact(optixContext,
-        /*stream:*/ 0,
-        meshHandle,
-        asBuffer.d_pointer(),
-        asBuffer.sizeInBytes,
-        &meshHandle));
+                                  nullptr,
+                                  meshHandle,
+                                  compactedGASBuffer.d_pointer(),
+                                  compactedGASBuffer.sizeInBytes,
+                                  &meshHandle));
     CUDA_SYNC_CHECK();
 
-    tempBuffer.free();
+    fullGASBuffer.free();
+    tempGASBuffer.free();
     compactedSizeBuffer.free();
-    needs_rebuild = false;
-
-    return meshHandle;
 }
 
 OptixInstance ModelInstance::buildIAS(unsigned int id)
 {
-    instance = {};
+    // NOTE: this assumes a single SBT record per GAS
+    OptixInstance instance = {
+        .instanceId = id,
+        .sbtOffset = id * LIDAR_RAY_TYPE_COUNT,
+        .visibilityMask = 255,
+        .flags = OPTIX_INSTANCE_FLAG_DISABLE_ANYHIT,
+        .traversableHandle = meshHandle,
+    };
     memcpy(instance.transform, m_triangle_mesh->transform.matrix_flat, sizeof(float) * 12);
-    // auto t = m_triangle_mesh->transform.matrix_flat;
-    // std::cout << "Transform: \n"
-    //     << t[0] << " " << t[1] << " " << t[2] << " " << t[3] << std::endl
-    //     << t[4] << " " << t[5] << " " << t[6] << " " << t[7] << std::endl
-    //     << t[8] << " " << t[9] << " " << t[10] << " " << t[11] << std::endl;
-    instance.instanceId = id;
-    instance.visibilityMask = 255;
-    // TODO(prybicki): this assumes a single SBT record per GAS
-    instance.sbtOffset = id * LIDAR_RAY_TYPE_COUNT; // This controls the SBT instance offset!
-    instance.flags = OPTIX_INSTANCE_FLAG_DISABLE_ANYHIT;
-    instance.traversableHandle = meshHandle;
     return instance;
 }
 
@@ -153,5 +113,4 @@ ModelInstance::~ModelInstance()
     m_normal_buffer.free();
     m_texcoord_buffer.free();
     m_index_buffer.free();
-    outputBuffer.free();
 }
