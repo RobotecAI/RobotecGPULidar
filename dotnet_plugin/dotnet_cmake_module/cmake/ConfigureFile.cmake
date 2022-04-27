52c4fd0cf44250b7825501b569076769cd3f6154
Piotr Rybicki <piotr.rybicki@robotec.ai>
2022-04-27 08:16:41 +0000

    Remove legacy API

--- dotnet_plugin/dotnet_cmake_module/cmake/ConfigureFile.cmake
+++
@@ -1,6 +0,0 @@
-#
-# Configure a file as a build step
-#
-
-
-configure_file(${CONFIG_INPUT_FILE} ${CONFIG_OUTPUT_FILE} @ONLY)
--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ E2E_DIR=$2
 INSTALL_DIR=${E2E_DIR}/Assets/E2ESimulator/Sensors/RobotecGPULidar/SimpleLidarAsset/Plugins
 
 echo "Installing from ${BUILD_DIR} to ${E2E_DIR}" &&
-cp ${BUILD_DIR}/dotnet_plugin/gpu-lidar-raycaster-dotnet/gpu_lidar_raycaster_dotnet.dll ${INSTALL_DIR}/ &&
 cp ${BUILD_DIR}/libRobotecGPULidar.so ${INSTALL_DIR}/Linux/x86_64/ &&
 echo "Install done"
 
