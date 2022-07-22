796f6f2223c40838389a9abf4fe2b8d140714300
Łukasz Chojnacki <lukasz.chojnacki@robotec.ai>
2022-07-22 17:24:40 +0200

    Add integration test (#1)
    * Add some integration tests: laser range, laser gaussian noise, mesh
    
    * WIP: integration test
    
    * Add delete_mesh and set_vertices integration tests
    
    * Add *_destroy to the end of test
    
    * Add transforms integration test
    
    * Add ray transform test
    
    * Add ray transform tests with gaussian noise
    
    * Integration test code refactor after review
    
    * Change test name from OneMeshManyEntities to Mesh

---
+++ test/src/integration/lidar/gaussianNoise.cpp
@@ -0,0 +1,154 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <rgl/api/e2e_extensions.h>
+#include <rgl/api/experimental.h>
+#include <testModels.hpp>
+#include <utils/statistical_utils.h>
+#include <utils/testUtils.h>
+
+class GaussianNoise : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        for (int i = 0; i < rays_count; ++i) {
+            rays_tf.push_back(ray_tf);
+        }
+
+        rgl_mesh_create(&cube_mesh, cube_vertices, cube_vertices_length, cube_indices, cube_indices_length);
+        rgl_entity_create(&cube, nullptr, cube_mesh);
+        rgl_entity_set_pose(cube, &entity_tf);
+        rgl_lidar_create(&lidar, rays_tf.data(), rays_tf.size());
+    }
+
+    void TearDown() override
+    {
+        rgl_mesh_destroy(cube_mesh);
+        rgl_entity_destroy(cube);
+        rgl_lidar_destroy(lidar);
+    }
+
+    float expected_hitpoint_z = 4.0;
+    float expected_angle_in_radians = 1.57;
+    std::vector<float> means = { -0.1, 0.0, 0.1 };
+    float distance_std_dev = 0.1;
+    float angle_std_dev = 0.01;
+    float std_dev_rise_per_meter = 0.2;
+    float abs_error = 0.002;
+
+    rgl_mesh_t cube_mesh;
+    rgl_entity_t cube;
+    rgl_lidar_t lidar;
+    rgl_mat3x4f entity_tf = {
+        .value = {
+            { 1, 0, 0, 0 },
+            { 0, 1, 0, 0 },
+            { 0, 0, 1, 5 } }
+    };
+    rgl_mat3x4f ray_tf = {
+        .value = {
+            { 1, 0, 0, 0 },
+            { 0, 1, 0, 0 },
+            { 0, 0, 1, 1 },
+        }
+    };
+    std::vector<rgl_mat3x4f> rays_tf;
+    int rays_count = 500000;
+    rgl_vec3f results[500000];
+    int hitpointCount = 0;
+};
+
+TEST_F(GaussianNoise, DistanceNoiseMean)
+{
+    for (auto mean : means) {
+        EXPECT_RGL_SUCCESS(rgl_lidar_set_gaussian_noise_params(lidar, rgl_angular_noise_type_t::RGL_ANGULAR_NOISE_TYPE_RAY_BASED, 0.0, 0.0, 0.0, 0.0, mean));
+        getLidarResults(lidar, &hitpointCount, results);
+
+        ASSERT_EQ(hitpointCount, rays_count);
+        for (int i = 0; i < rays_count; ++i) {
+            EXPECT_EQ(results[i].value[0], 0.0f);
+            EXPECT_EQ(results[i].value[1], 0.0f);
+            EXPECT_EQ(results[i].value[2], expected_hitpoint_z + mean);
+        }
+    }
+}
+
+TEST_F(GaussianNoise, DistanceStandardDeviation)
+{
+    EXPECT_RGL_SUCCESS(rgl_lidar_set_gaussian_noise_params(lidar, rgl_angular_noise_type_t::RGL_ANGULAR_NOISE_TYPE_RAY_BASED, 0.0, 0.0, distance_std_dev, 0.0, 0.0));
+    getLidarResults(lidar, &hitpointCount, results);
+
+    ASSERT_EQ(hitpointCount, rays_count);
+    auto distances = computeDistances(results, rays_count);
+    auto [distance_mean, distance_st_dev] = mean_and_stdev(distances);
+    EXPECT_NEAR(distance_mean, expected_hitpoint_z, abs_error);
+    EXPECT_NEAR(distance_st_dev, distance_std_dev, abs_error);
+}
+
+TEST_F(GaussianNoise, DistanceStandardDeviationRaisePerMeter)
+{
+    EXPECT_RGL_SUCCESS(rgl_lidar_set_gaussian_noise_params(lidar, rgl_angular_noise_type_t::RGL_ANGULAR_NOISE_TYPE_RAY_BASED, 0.0, 0.0, distance_std_dev, std_dev_rise_per_meter, 0.0));
+    getLidarResults(lidar, &hitpointCount, results);
+
+    ASSERT_EQ(hitpointCount, rays_count);
+    auto distances = computeDistances(results, rays_count);
+    auto [distance_mean, distance_st_dev] = mean_and_stdev(distances);
+    EXPECT_NEAR(distance_mean, expected_hitpoint_z, abs_error);
+    EXPECT_NEAR(distance_st_dev, distance_std_dev, abs_error);
+    // TODO: investigate why this EXPECT do not pass
+    // EXPECT_NEAR(distance_st_dev, distance_std_dev + expected_hitpoint_z * std_dev_rise_per_meter, 0.0002f);
+}
+
+TEST_F(GaussianNoise, AngleNoiseRayBasedStandardDeviation)
+{
+    EXPECT_RGL_SUCCESS(rgl_lidar_set_gaussian_noise_params(lidar, rgl_angular_noise_type_t::RGL_ANGULAR_NOISE_TYPE_RAY_BASED, angle_std_dev, 0.0, 0.0, 0.0, 0.0));
+    getLidarResults(lidar, &hitpointCount, results);
+
+    ASSERT_EQ(hitpointCount, rays_count);
+    auto angles = computeAngles(results, rays_count);
+    auto [angle_mean, angle_std_dev] = mean_and_stdev(angles);
+    EXPECT_NEAR(angle_mean, expected_angle_in_radians, abs_error);
+    EXPECT_NEAR(angle_std_dev, angle_std_dev, abs_error);
+}
+
+TEST_F(GaussianNoise, AngleNoiseRayBasedMean)
+{
+    for (auto mean : means) {
+
+        EXPECT_RGL_SUCCESS(rgl_lidar_set_gaussian_noise_params(lidar, rgl_angular_noise_type_t::RGL_ANGULAR_NOISE_TYPE_RAY_BASED, 0.0, mean, 0.0, 0.0, 0.0));
+        getLidarResults(lidar, &hitpointCount, results);
+
+        ASSERT_EQ(hitpointCount, rays_count);
+        auto angles = computeAngles(results, rays_count);
+        auto [angle_mean, angle_std_dev] = mean_and_stdev(angles);
+        // TODO: investigate why this EXPECT do not pass
+        // EXPECT_NEAR(angle_mean, expected_angle_in_radians + mean, abs_error);
+        EXPECT_NEAR(angle_std_dev, 0.0f, abs_error);
+    }
+}
+
+TEST_F(GaussianNoise, AngleNoiseHitpointBasedStandardDeviation)
+{
+    EXPECT_RGL_SUCCESS(rgl_lidar_set_gaussian_noise_params(lidar, rgl_angular_noise_type_t::RGL_ANGULAR_NOISE_TYPE_HITPOINT_BASED, angle_std_dev, 0.0, 0.0, 0.0, 0.0));
+    getLidarResults(lidar, &hitpointCount, results);
+
+    ASSERT_EQ(hitpointCount, rays_count);
+    auto angles = computeAngles(results, rays_count);
+    auto [angle_mean, angle_std_dev] = mean_and_stdev(angles);
+    EXPECT_NEAR(angle_mean, expected_angle_in_radians, abs_error);
+    EXPECT_NEAR(angle_std_dev, angle_std_dev, abs_error);
+}
+
+TEST_F(GaussianNoise, AngleNoiseHitpointBasedMean)
+{
+    for (auto mean : means) {
+
+        EXPECT_RGL_SUCCESS(rgl_lidar_set_gaussian_noise_params(lidar, rgl_angular_noise_type_t::RGL_ANGULAR_NOISE_TYPE_HITPOINT_BASED, 0.0, mean, 0.0, 0.0, 0.0));
+        getLidarResults(lidar, &hitpointCount, results);
+
+        ASSERT_EQ(hitpointCount, rays_count);
+        auto angles = computeAngles(results, rays_count);
+        auto [angle_mean, angle_std_dev] = mean_and_stdev(angles);
+        EXPECT_NEAR(angle_mean, expected_angle_in_radians + mean, abs_error);
+        EXPECT_NEAR(angle_std_dev, 0.0f, abs_error);
+    }
+}
---
+++ test/src/integration/mesh.cpp
@@ -0,0 +1,87 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <rgl/api/experimental.h>
+#include <testModels.hpp>
+#include <utils/testUtils.h>
+
+using namespace ::testing;
+
+class Mesh : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        rgl_lidar_create(&lidar, &ray_tf, 1);
+    }
+
+    void TearDown() override
+    {
+        rgl_lidar_destroy(lidar);
+    }
+
+    rgl_mesh_t cube_mesh = nullptr;
+    rgl_entity_t entity = nullptr;
+    rgl_lidar_t lidar = nullptr;
+
+    rgl_mat3x4f entity_tf = {
+        .value = {
+            { 1, 0, 0, 0 },
+            { 0, 1, 0, 0 },
+            { 0, 0, 1, 5 } }
+    };
+    rgl_mat3x4f ray_tf = {
+        .value = {
+            { 1, 0, 0, 0 },
+            { 0, 1, 0, 0 },
+            { 0, 0, 1, 0 },
+        }
+    };
+
+    rgl_vec3f results[1];
+    int hitpointCount = 0;
+};
+
+TEST_F(Mesh, DeleteMesh)
+{
+    EXPECT_RGL_SUCCESS(rgl_mesh_create(&cube_mesh, cube_vertices, cube_vertices_length, cube_indices, cube_indices_length));
+    EXPECT_RGL_SUCCESS(rgl_entity_create(&entity, nullptr, cube_mesh));
+    EXPECT_RGL_SUCCESS(rgl_entity_set_pose(entity, &entity_tf));
+    getLidarResults(lidar, &hitpointCount, results);
+
+    EXPECT_EQ(hitpointCount, 1);
+    EXPECT_FLOAT_EQ(results[0].value[2], 4.0f);
+
+    EXPECT_RGL_SUCCESS(rgl_mesh_destroy(cube_mesh));
+    EXPECT_RGL_SUCCESS(rgl_entity_set_pose(entity, &entity_tf));
+
+    getLidarResults(lidar, &hitpointCount, results);
+
+    EXPECT_EQ(hitpointCount, 1);
+    EXPECT_FLOAT_EQ(results[0].value[2], 4.0f);
+
+    EXPECT_RGL_SUCCESS(rgl_entity_destroy(entity));
+    getLidarResults(lidar, &hitpointCount, results);
+
+    EXPECT_EQ(hitpointCount, 0);
+}
+
+TEST_F(Mesh, SetVertices)
+{
+    EXPECT_RGL_SUCCESS(rgl_mesh_create(&cube_mesh, cube_vertices, cube_vertices_length, cube_indices, cube_indices_length));
+    EXPECT_RGL_SUCCESS(rgl_entity_create(&entity, nullptr, cube_mesh));
+    EXPECT_RGL_SUCCESS(rgl_entity_set_pose(entity, &entity_tf));
+
+    getLidarResults(lidar, &hitpointCount, results);
+
+    EXPECT_EQ(hitpointCount, 1);
+    EXPECT_FLOAT_EQ(results[0].value[2], 4.0f);
+
+    EXPECT_RGL_SUCCESS(rgl_mesh_set_vertices(cube_mesh, cube_vertices_big, cube_vertices_big_length));
+
+    getLidarResults(lidar, &hitpointCount, results);
+
+    EXPECT_EQ(hitpointCount, 1);
+    EXPECT_FLOAT_EQ(results[0].value[2], 3.0f);
+
+    EXPECT_RGL_SUCCESS(rgl_mesh_destroy(cube_mesh));
+    EXPECT_RGL_SUCCESS(rgl_entity_destroy(entity));
+}
