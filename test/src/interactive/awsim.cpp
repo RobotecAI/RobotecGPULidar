d8d5e7535bb6de88db1630b6a49dc58295992b02
Mateusz Szczygielski <112629916+msz-rai@users.noreply.github.com>
2022-09-29 15:17:36 +0200

    Format node doesn't produce outToken (#22)

--- test/src/interactive/awsim.cpp
+++ test/src/interactive/awsim.cpp
@@ -35,15 +35,14 @@ int main(int argc, char** argv)
 	rgl_node_t raysNode=nullptr, yield=nullptr, raytrace=nullptr, poseNode=nullptr, ros=nullptr, compact=nullptr, fmt24=nullptr, fmt48=nullptr;
 	std::vector<rgl_mat3x4f> rays;
 	rgl_mat3x4f lidarPose = Mat3x4f::translation(0, 0, 0).toRGL();
-	rgl_field_t fmt24Token, fmt48Token;
 
 	CHECK_RGL(rgl_node_use_rays_mat3x4f(&raysNode, rays.data(), rays.size()));
 	CHECK_RGL(rgl_node_transform_rays(&poseNode, &lidarPose));
 	CHECK_RGL(rgl_node_raytrace(&raytrace, nullptr, 1000.0f));
 	CHECK_RGL(rgl_node_compact(&compact));
 		CHECK_RGL(rgl_node_yield_points(&yield, visFields.data(), visFields.size()));
-		CHECK_RGL(rgl_node_format(&fmt24, &fmt24Token, pcl24Fields.data(), pcl24Fields.size()));
-		CHECK_RGL(rgl_node_format(&fmt48, &fmt48Token, pcl48Fields.data(), pcl48Fields.size()));
+		CHECK_RGL(rgl_node_format(&fmt24, pcl24Fields.data(), pcl24Fields.size()));
+		CHECK_RGL(rgl_node_format(&fmt48, pcl48Fields.data(), pcl48Fields.size()));
 
 	CHECK_RGL(rgl_graph_node_add_child(raysNode, poseNode));
 	CHECK_RGL(rgl_graph_node_add_child(poseNode, raytrace));
