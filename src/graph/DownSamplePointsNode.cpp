c0186ffff9c8a5ad747bfe7821b5886430d3e96a
Mateusz Szczygielski <112629916+msz-rai@users.noreply.github.com>
2022-10-10 12:35:06 +0200

    Handle zero-sized input for DownSamplePointsNode and VisualizePointsNode (#31)
    * Handle zero-sized input
    
    * Apply to VisualizePointsNode also

--- src/graph/DownSamplePointsNode.cpp
+++ src/graph/DownSamplePointsNode.cpp
@@ -18,6 +18,11 @@ void DownSamplePointsNode::schedule(cudaStream_t stream)
 {
 	cacheManager.trigger();
 
+	if (input->getPointCount() == 0) {
+		filteredIndices->resize(0, false, false);
+		return;
+	}
+
 	// Get formatted input data
 	FormatPointsNode::formatAsync(inputFmtData, input, requiredFields, stream);
 
