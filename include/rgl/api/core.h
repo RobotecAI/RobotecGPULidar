01134804553151c24b3efcb02330013c0ac1b44c
Piotr Rybicki <piotr.rybicki@robotec.ai>
2022-11-09 12:51:41 +0100

    Fix visibility macro issue

--- include/rgl/api/core.h
+++ include/rgl/api/core.h
@@ -1,9 +1,31 @@
 #pragma once
 
-#include <macros/visibility.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+
+#ifdef __cplusplus
+#define NO_MANGLING extern "C"
+#else // NOT __cplusplus
+#define NO_MANGLING
+#endif
+
+#if defined _WIN32 || defined __CYGWIN__
+#ifdef __GNUC__
+  #define RGL_VISIBLE __attribute__ ((dllexport))
+ #else
+  #define RGL_VISIBLE __declspec(dllexport)
+ #endif // __GNUC__
+#else
+#define RGL_VISIBLE __attribute__ ((visibility("default")))
+#if __GNUC__ >= 4
+#define RGL_VISIBLE __attribute__ ((visibility("default")))
+#else
+#define RGL_VISIBLE
+#endif
+#endif // _WIN32 || __CYGWIN__
+
+#define RGL_API NO_MANGLING RGL_VISIBLE
 
 #define RGL_VERSION_MAJOR 0
 #define RGL_VERSION_MINOR 11
