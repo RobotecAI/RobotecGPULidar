893c1fce01e7d1b87adc935128f973b9335c3449
Mateusz Szczygielski <mateusz.szczygielski@robotec.ai>
2022-11-30 17:18:06 +0100

    Fix double static initialization

--- src/api/apiCommon.hpp
+++ src/api/apiCommon.hpp
@@ -33,67 +33,16 @@ do if (!(expr)) {                                                               
     throw std::invalid_argument(msg);                                                            \
 } while(0)
 
-static rgl_status_t lastStatusCode = RGL_SUCCESS;
-static std::optional<std::string> lastStatusString = std::nullopt;
+extern rgl_status_t lastStatusCode;
+extern std::optional<std::string> lastStatusString;
 
-static bool isCompiledWithAutoTape() { return !std::string(RGL_AUTO_TAPE_PATH).empty(); }
-
-static void rglLazyInit();
-
-static const char* getLastErrorString()
-{
-	// By default, use lastStatusString value, which is usually a message from the caught exception
-	if (lastStatusString.has_value()) {
-		return lastStatusString->c_str();
-	}
-	// If lastStatusString is not set, provide a fallback explanation:
-	switch (lastStatusCode) {
-		case RGL_SUCCESS: return "operation successful";
-		case RGL_INVALID_ARGUMENT: return "invalid argument";
-		case RGL_INTERNAL_EXCEPTION: return "unhandled internal exception";
-		case RGL_INVALID_STATE: return "invalid state - unrecoverable error occurred";
-		case RGL_NOT_IMPLEMENTED: return "operation not (yet) implemented";
-		case RGL_LOGGING_ERROR: return "spdlog error";
-		case RGL_INVALID_FILE_PATH: return "invalid file path";
-		case RGL_TAPE_ERROR: return "tape error";
-		default: return "???";
-	}
-}
-
-static bool canContinueAfterStatus(rgl_status_t status)
-{
-	// Set constructor may throw, hence lazy initialization.
-	static std::set recoverableErrors = {
-		RGL_INVALID_ARGUMENT,
-		RGL_INVALID_API_OBJECT,
-		RGL_INVALID_PIPELINE,
-		RGL_INVALID_FILE_PATH,
-		RGL_NOT_IMPLEMENTED,
-		RGL_TAPE_ERROR
-	};
-	return status == RGL_SUCCESS || recoverableErrors.contains(status);
-};
-
-static rgl_status_t updateAPIState(rgl_status_t status, std::optional<std::string> auxErrStr = std::nullopt)
-{
-	lastStatusString = auxErrStr;
-	lastStatusCode = status;
-	if (status != RGL_SUCCESS) {
-		// Report API error in log file
-		const char* msg = getLastErrorString();
-		if (canContinueAfterStatus(lastStatusCode)) {
-			RGL_ERROR("Recoverable error (code={}): {}", lastStatusCode, msg);
-		}
-		else {
-			RGL_CRITICAL("Unrecoverable error (code={}): {}", lastStatusCode, msg);
-		}
-		Logger::getOrCreate().flush();
-	}
-	return status;
-}
+const char* getLastErrorString();
+bool canContinueAfterStatus(rgl_status_t status);
+rgl_status_t updateAPIState(rgl_status_t status, std::optional<std::string> auxErrStr = std::nullopt);
+void rglLazyInit();
 
 template<typename Fn>
-static rgl_status_t rglSafeCall(Fn fn)
+rgl_status_t rglSafeCall(Fn fn)
 {
 	rglLazyInit(); // Trigger initialization on the first API call
 	if (!canContinueAfterStatus(lastStatusCode)) {
@@ -134,28 +83,6 @@ static rgl_status_t rglSafeCall(Fn fn)
 		return updateAPIState(RGL_INTERNAL_EXCEPTION, "exceptional exception");
 	}
 	return updateAPIState(RGL_SUCCESS);
-}
-
-static void rglLazyInit()
-{
-	static bool initCalled = false;
-	if (initCalled) {
-		return;
-	}
-	initCalled = true;
-	rgl_status_t initStatus = rglSafeCall([&]() {
-		Logger::getOrCreate();
-		Optix::getOrCreate();
-		if (isCompiledWithAutoTape()) {
-			auto path = std::filesystem::path(RGL_AUTO_TAPE_PATH);
-			RGL_INFO("Starting RGL Auto Tape on path '{}'", path.string());
-			tapeRecord.emplace(path);
-		}
-	});
-	if (initStatus != RGL_SUCCESS) {
-		// If initialization fails, change error code to unrecoverable one and preserve original message
-		updateAPIState(RGL_INITIALIZATION_ERROR, lastStatusString);
-	}
 }
 
 template<typename NodeType, typename... Args>
