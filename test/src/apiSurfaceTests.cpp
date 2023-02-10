eac6066b2e143fa97331e18650f36e6b5327a392
nebraszka <maja.nagarnowicz@robotec.ai>
2023-02-10 10:14:31 +0100

    Add api surface graph tests
    * Add api surface graph tests
    * Create new macros in the utils.hpp
    * Change of some comments in the api surface tests

--- test/src/apiSurfaceTests.cpp
+++ test/src/apiSurfaceTests.cpp
@@ -43,7 +43,6 @@ TEST_F(APISurfaceTests, rgl_get_last_error_string)
 	Logger::getOrCreate().flush();
 
 	// Expected log should be written in the file
-	// TODO(nebraszka): Is it possible to handle access to the log file name more nicely?
 	std::filesystem::path logFilePath { std::filesystem::temp_directory_path() / std::filesystem::path(FILENAME).concat(".log") };
 	ASSERT_THAT(std::filesystem::exists(logFilePath), IsTrue());
 	std::string logFile = readFileStr(logFilePath);
@@ -76,11 +75,11 @@ TEST_F(APISurfaceTests, rgl_mesh_create_destroy)
 	ASSERT_RGL_SUCCESS(rgl_mesh_destroy(mesh));
 
 	// Double destroy
-	EXPECT_RGL_INVALID_OBJECT(rgl_mesh_destroy(mesh), "Object does not exist: Mesh");
+	EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_mesh_destroy(mesh), "Object does not exist: Mesh");
 
 	// Invalid destroy
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_mesh_destroy(nullptr), "mesh != nullptr");
-	EXPECT_RGL_INVALID_OBJECT(rgl_mesh_destroy((rgl_mesh_t) 0x1234), "Object does not exist: Mesh 0x1234");
+	EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_mesh_destroy((rgl_mesh_t) 0x1234), "Object does not exist: Mesh 0x1234");
 }
 
 TEST_F(APISurfaceTests, rgl_mesh_update_vertices)
@@ -92,7 +91,7 @@ TEST_F(APISurfaceTests, rgl_mesh_update_vertices)
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_mesh_update_vertices(mesh,    nullptr,  0), "vertices != nullptr");
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_mesh_update_vertices(mesh,    VERTICES, 0), "vertex_count > 0");
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_mesh_update_vertices(mesh,    VERTICES, ARRAY_SIZE(VERTICES) + 1), "vertex counts do not match");
-	EXPECT_RGL_INVALID_OBJECT(rgl_mesh_update_vertices((rgl_mesh_t) 0x1234, VERTICES, ARRAY_SIZE(VERTICES)), "Object does not exist: Mesh 0x1234");
+	EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_mesh_update_vertices((rgl_mesh_t) 0x1234, VERTICES, ARRAY_SIZE(VERTICES)), "Object does not exist: Mesh 0x1234");
 
 	// Correct update_vertices
 	EXPECT_RGL_SUCCESS(rgl_mesh_update_vertices(mesh, VERTICES, ARRAY_SIZE(VERTICES)));
@@ -106,8 +105,8 @@ TEST_F(APISurfaceTests, rgl_entity_create_destroy)
 	// Invalid args, note: scene can be nullptr here.
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_entity_create(nullptr, nullptr, nullptr), "entity != nullptr");
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_entity_create(&entity, nullptr, nullptr), "mesh != nullptr");
-	EXPECT_RGL_INVALID_OBJECT(rgl_entity_create(&entity, (rgl_scene_t) 0x1234, mesh), "Object does not exist: Scene 0x1234");
-	EXPECT_RGL_INVALID_OBJECT(rgl_entity_create(&entity, nullptr, (rgl_mesh_t) 0x1234), "Object does not exist: Mesh 0x1234");
+	EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_entity_create(&entity, (rgl_scene_t) 0x1234, mesh), "Object does not exist: Scene 0x1234");
+	EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_entity_create(&entity, nullptr, (rgl_mesh_t) 0x1234), "Object does not exist: Mesh 0x1234");
 
 	// Correct create
 	ASSERT_RGL_SUCCESS(rgl_entity_create(&entity, nullptr, mesh));
@@ -117,11 +116,11 @@ TEST_F(APISurfaceTests, rgl_entity_create_destroy)
 	ASSERT_RGL_SUCCESS(rgl_entity_destroy(entity));
 
 	// Double destroy
-	EXPECT_RGL_INVALID_OBJECT(rgl_entity_destroy(entity), "Object does not exist: Entity");
+	EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_entity_destroy(entity), "Object does not exist: Entity");
 
 	// Invalid destroy
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_entity_destroy(nullptr), "entity != nullptr");
-	EXPECT_RGL_INVALID_OBJECT(rgl_entity_destroy((rgl_entity_t) 0x1234), "Object does not exist: Entity 0x1234");
+	EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_entity_destroy((rgl_entity_t) 0x1234), "Object does not exist: Entity 0x1234");
 }
 
 TEST_F(APISurfaceTests, rgl_entity_set_pose)
@@ -131,7 +130,7 @@ TEST_F(APISurfaceTests, rgl_entity_set_pose)
 	// Invalid args
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_entity_set_pose(nullptr, nullptr), "entity != nullptr");
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_entity_set_pose(entity,  nullptr), "transform != nullptr");
-	EXPECT_RGL_INVALID_OBJECT(rgl_entity_set_pose((rgl_entity_t) 0x1234, &identity), "Object does not exist: Entity 0x1234");
+	EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_entity_set_pose((rgl_entity_t) 0x1234, &identity), "Object does not exist: Entity 0x1234");
 
 	// Correct set_pose
 	EXPECT_RGL_SUCCESS(rgl_entity_set_pose(entity, &identity));
@@ -146,7 +145,7 @@ TEST_F(APISurfaceTests, rgl_node_rays_from_mat3x4f)
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_rays_from_mat3x4f(&node, 	nullptr,   0), "rays != nullptr");
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_rays_from_mat3x4f(&node, 	&identity, 0), "ray_count > 0");
 
-	// Valid args
+	// Correct node_rays_from_mat3x4f
 	EXPECT_RGL_SUCCESS(rgl_node_rays_from_mat3x4f(&node, &identity, 1));
 	ASSERT_THAT(node, NotNull());
 
@@ -164,7 +163,7 @@ TEST_F(APISurfaceTests, rgl_node_rays_set_ring_ids)
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_rays_set_ring_ids(&node, 	nullptr,  0), "ring_ids != nullptr");
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_rays_set_ring_ids(&node, 	ring_ids, 0), "ring_ids_count > 0");
 
-	// Valid args
+	// Correct set_ring_ids
 	EXPECT_RGL_SUCCESS(rgl_node_rays_set_ring_ids(&node, ring_ids, 1));
 	ASSERT_THAT(node, NotNull());
 
@@ -180,7 +179,7 @@ TEST_F(APISurfaceTests, rgl_node_rays_transform)
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_rays_transform(nullptr, nullptr), "node != nullptr");
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_rays_transform(&node,	 nullptr), "transform != nullptr");
 
-	// Valid args
+	// Correct rays_transform
 	EXPECT_RGL_SUCCESS(rgl_node_rays_transform(&node, &identity));
 	ASSERT_THAT(node, NotNull());
 
@@ -196,7 +195,7 @@ TEST_F(APISurfaceTests, rgl_node_points_transform)
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_transform(nullptr, nullptr), "node != nullptr");
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_transform(&node,   nullptr), "transform != nullptr");
 
-	// Valid args
+	// Correct points_transform
 	EXPECT_RGL_SUCCESS(rgl_node_points_transform(&node, &identity));
 	ASSERT_THAT(node, NotNull());
 
@@ -210,9 +209,9 @@ TEST_F(APISurfaceTests, rgl_node_raytrace)
 
 	// Invalid args
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_raytrace(nullptr, nullptr, 0.0f), "node != nullptr");
-	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_raytrace(&node, nullptr, 0.0f), "range > 0.0f");
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_raytrace(&node,   nullptr, 0.0f), "range > 0.0f");
 
-	// Valid args
+	// Correct raytrace
 	EXPECT_RGL_SUCCESS(rgl_node_raytrace(&node, nullptr, 1.0f));
 	ASSERT_THAT(node, NotNull());
 
@@ -227,10 +226,10 @@ TEST_F(APISurfaceTests, rgl_node_points_format)
 
 	// Invalid args
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_format(nullptr, nullptr, 0), "node != nullptr");
-	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_format(&node, nullptr, 0), "fields != nullptr");
-	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_format(&node, &fields, 0), "field_count > 0");
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_format(&node,   nullptr, 0), "fields != nullptr");
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_format(&node, 	&fields, 0), "field_count > 0");
 
-	// Valid args
+	// Correct points_format
 	EXPECT_RGL_SUCCESS(rgl_node_points_format(&node, &fields, 1));
 	ASSERT_THAT(node, NotNull());
 
@@ -245,10 +244,10 @@ TEST_F(APISurfaceTests, rgl_node_points_yield)
 
 	// Invalid args
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_yield(nullptr, nullptr, 0), "node != nullptr");
-	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_yield(&node, nullptr, 0), "fields != nullptr");
-	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_yield(&node, &fields, 0), "field_count > 0");
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_yield(&node,   nullptr, 0), "fields != nullptr");
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_yield(&node,   &fields, 0), "field_count > 0");
 
-	// Valid args
+	// Correct points_yield
 	EXPECT_RGL_SUCCESS(rgl_node_points_yield(&node, &fields, 1));
 	ASSERT_THAT(node, NotNull());
 
@@ -260,10 +259,10 @@ TEST_F(APISurfaceTests, rgl_node_points_compact)
 {
 	rgl_node_t node = nullptr;
 
-	// Invalid args
+	// Invalid arg (node)
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_compact(nullptr), "node != nullptr");
 
-	// Valid args
+	// Correct points_compact
 	EXPECT_RGL_SUCCESS(rgl_node_points_compact(&node));
 	ASSERT_THAT(node, NotNull());
 
@@ -275,12 +274,183 @@ TEST_F(APISurfaceTests, rgl_node_points_downsample)
 {
 	rgl_node_t node = nullptr;
 
-	// Invalid args
+	// Invalid arg (node)
 	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_downsample(nullptr, 0.0f, 0.0f, 0.0f), "node != nullptr");
-	// EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_downsample(&node, 0.0f, 0.0f, 0.0f), "leaf_size_x > 0.0f");
-	// EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_downsample(&node, 1.0f, 0.0f, 0.0f), "leaf_size_y > 0.0f");
-	// EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_downsample(&node, 1.0f, 1.0f, 0.0f), "leaf_size_z > 0.0f");
+
+	// Correct points_downsample
+	EXPECT_RGL_SUCCESS(rgl_node_points_downsample(&node, 1.0f, 1.0f, 1.0f));
+
+	// If (*node) != nullptr
+	EXPECT_RGL_SUCCESS(rgl_node_points_downsample(&node, 2.0f, 2.0f, 2.0f));
+}
+
+TEST_F(APISurfaceTests, rgl_graph_run)
+{
+	rgl_node_t node = nullptr, raytrace = nullptr;
+
+	// Invalid arg (node)
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_run(nullptr), "node != nullptr");
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_run(node), "node != nullptr");
+
+	// Creating graph with two nodes
+	ASSERT_RGL_SUCCESS(rgl_node_rays_from_mat3x4f(&node, &identity, 1));
+	ASSERT_RGL_SUCCESS(rgl_node_raytrace(&raytrace, nullptr, 1000));
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(node, raytrace));
+
+	// Correct graph_run
+	EXPECT_RGL_SUCCESS(rgl_graph_run(node));
+}
+
+TEST_F(APISurfaceTests, rgl_graph_destroy)
+{
+	rgl_node_t ray_node = nullptr, raytrace_node = nullptr;
+
+	// Invalid arg (node)
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_destroy(nullptr), "node != nullptr");
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_destroy(ray_node), "node != nullptr");
+	EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_graph_destroy((rgl_node_t) 0x1234), "Object does not exist: Node 0x1234");
+
+	// Creating nodes
+	ASSERT_RGL_SUCCESS(rgl_node_rays_from_mat3x4f(&ray_node, &identity, 1));
+	ASSERT_RGL_SUCCESS(rgl_node_raytrace(&raytrace_node, nullptr, 1000));
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(ray_node, raytrace_node));
+
+	// Correct destroy
+	ASSERT_RGL_SUCCESS(rgl_graph_destroy(ray_node));
+
+	// Double destroy
+	EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_graph_destroy(raytrace_node), "Object does not exist: Node");
+	EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_graph_destroy(ray_node), "Object does not exist: Node");
+}
+
+TEST_F(APISurfaceTests, rgl_graph_get_result_size)
+{
+	rgl_node_t ray_node = nullptr, raytrace_node = nullptr;
+	int32_t hitpoint_count, point_size;
+
+	// Invalid arg (node)
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_get_result_size(nullptr, RGL_FIELD_XYZ_F32, &hitpoint_count, &point_size), "node != nullptr");
+	EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_graph_get_result_size((rgl_node_t) 0x1234, RGL_FIELD_XYZ_F32, &hitpoint_count, &point_size), "Object does not exist: Node 0x1234");
+
+	// Creating graph with two nodes
+	ASSERT_RGL_SUCCESS(rgl_node_rays_from_mat3x4f(&ray_node, &identity, 1));
+	ASSERT_RGL_SUCCESS(rgl_node_raytrace(&raytrace_node, nullptr, 1000));
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(ray_node, raytrace_node));
+
+	// Correct get_result_size (without prior running the graph)
+	// EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(raytrace_node, RGL_FIELD_XYZ_F32, nullptr, nullptr));
+	// EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(raytrace_node, RGL_FIELD_XYZ_F32, &hitpoint_count, &point_size));
+
+	// Correct get_result_size (with prior running the graph)
+	ASSERT_RGL_SUCCESS(rgl_graph_run(raytrace_node));
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(raytrace_node, RGL_FIELD_XYZ_F32, nullptr, nullptr));
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(raytrace_node, RGL_FIELD_XYZ_F32, &hitpoint_count, &point_size));
+}
+
+TEST_F(APISurfaceTests, rgl_graph_get_result_data)
+{
+	rgl_node_t ray_node = nullptr, raytrace_node = nullptr;
+	int32_t hitpoint_count, point_size;
+	rgl_vec3f results[1];
+
+	// Invalid arg (node)
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_get_result_data(nullptr, 	RGL_FIELD_XYZ_F32, nullptr), "node != nullptr");
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_get_result_data(ray_node, RGL_FIELD_XYZ_F32, nullptr), "node != nullptr");
+	EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_graph_get_result_data((rgl_node_t) 0x1234, RGL_FIELD_XYZ_F32, &results), "Object does not exist: Node 0x1234");
+
+	// Invalid arg (data)
+	ASSERT_RGL_SUCCESS(rgl_node_rays_from_mat3x4f(&ray_node, &identity, 1));
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_get_result_data(ray_node, RGL_FIELD_XYZ_F32, nullptr), "data != nullptr");
+
+	// Creating graph with two nodes
+	ASSERT_RGL_SUCCESS(rgl_node_raytrace(&raytrace_node, nullptr, 1000));
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(ray_node, raytrace_node));
+
+	// Incorrect type of node
+	EXPECT_RGL_NODE_TYPE_MISMATCH(rgl_graph_get_result_data(ray_node, RGL_FIELD_XYZ_F32, &results), "expected IPointsNode, got FromMat3x4fRaysNode");
+
+	// Correct get_result_data (without prior running the graph)
+	// EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_get_result_data(raytrace_node, RGL_FIELD_XYZ_F32, &results), "");
+
+	// Correct get_result_data (with prior running the graph)
+	ASSERT_RGL_SUCCESS(rgl_graph_run(raytrace_node));
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_data(raytrace_node, RGL_FIELD_XYZ_F32, &results));
+}
+
+TEST_F(APISurfaceTests, rgl_graph_node_set_active)
+{
+	rgl_node_t ray_node = nullptr;
+
+	// Invalid arg (node)
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_node_set_active(nullptr, 	0), "node != nullptr");
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_node_set_active(ray_node, 0), "node != nullptr");
+	// EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_graph_node_set_active((rgl_node_t) 0x1234, 1), "Object does not exist: Node 0x1234");
+
+	// Creating node
+	ASSERT_RGL_SUCCESS(rgl_node_rays_from_mat3x4f(&ray_node, &identity, 1));
+
+	// Correct node_set_active
+	EXPECT_RGL_SUCCESS(rgl_graph_node_set_active(ray_node, 0));
+	EXPECT_RGL_SUCCESS(rgl_graph_node_set_active(ray_node, 1));
+}
+
+TEST_F(APISurfaceTests, rgl_graph_node_add_child)
+{
+	rgl_node_t parent = nullptr, child = nullptr;
+
+	// Invalid arg (parent)
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_node_add_child(nullptr, nullptr), "parent != nullptr");
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_node_add_child(parent,  nullptr), "parent != nullptr");
+
+	// Creating rays node - parent
+	ASSERT_RGL_SUCCESS(rgl_node_rays_from_mat3x4f(&parent, &identity, 1));
+
+	// Invalid arg (child)
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_node_add_child(parent,  nullptr), "child != nullptr");
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_node_add_child(parent,  child), 	"child != nullptr");
+	//EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_graph_node_add_child(parent, (rgl_node_t) 0x1234), "Object does not exist: Node 0x1234");
+
+	// Creating raytrace node - child
+	ASSERT_RGL_SUCCESS(rgl_node_raytrace(&child, nullptr, 1000));
+
+	// Invalid arg (parent does not exist)
+	//EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_graph_node_add_child((rgl_node_t) 0x1234, child), "Object does not exist: Node 0x1234");
+
+	// Correct add_child
+	EXPECT_RGL_SUCCESS(rgl_graph_node_add_child(parent, child));
+}
+
+TEST_F(APISurfaceTests, rgl_graph_node_remove_child)
+{
+	rgl_node_t parent = nullptr, child = nullptr;
+
+	// Invalid arg (parent)
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_node_remove_child(nullptr, nullptr), "parent != nullptr");
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_node_remove_child(parent,  nullptr), "parent != nullptr");
+
+	// Creating rays node
+	ASSERT_RGL_SUCCESS(rgl_node_rays_from_mat3x4f(&parent, &identity, 1));
+
+	// Invalid arg (child)
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_node_remove_child(parent,  nullptr),  "child != nullptr");
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_graph_node_remove_child(parent,  child), 	"child != nullptr");
+	EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_graph_node_remove_child(parent, (rgl_node_t) 0x1234), "Object does not exist: Node 0x1234");
+
+	// Creating raytrace node
+	ASSERT_RGL_SUCCESS(rgl_node_raytrace(&child, nullptr, 1000));
+
+	// Invalid arg (parent does not exist)
+	//EXPECT_RGL_OBJECT_NOT_EXISTS(rgl_graph_node_remove_child((rgl_node_t) 0x1234, child), "Object does not exist: Node 0x1234");
+
+	// Invalid args - nodes are not connected
+	EXPECT_RGL_INVALID_PIPELINE(rgl_graph_node_remove_child(parent, child), "RaytraceNode from FromMat3x4fRaysNode");
+
+	// Connecting nodes
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(parent, child));
+
+	// Invalid args - incorrect order
+	EXPECT_RGL_INVALID_PIPELINE(rgl_graph_node_remove_child(child, parent), "FromMat3x4fRaysNode from RaytraceNode");
 
 	// Valid args
-	EXPECT_RGL_SUCCESS(rgl_node_points_downsample(&node, 1.0f, 1.0f, 1.0f));
+	EXPECT_RGL_SUCCESS(rgl_graph_node_remove_child(parent, child));
 }
