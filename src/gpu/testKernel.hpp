27130bc91949562a874f6bdc110f6b3ae6b949dc
Piotr Mrozik <piter012@vp.pl>
2023-03-20 12:04:23 +0100

    Cleanup header and cmake

--- src/gpu/testKernel.hpp
+++ src/gpu/testKernel.hpp
@@ -1,12 +1,3 @@
 #pragma once
 
-#include <unordered_map>
-#include <memory>
-
-#include <rgl/api/core.h>
-#include <gpu/GPUFieldDesc.hpp>
-#include <math/Mat3x4f.hpp>
-#include <RGLFields.hpp>
-
 void testKernelWrapper(size_t pointCount, float* inFloats, cudaStream_t stream);
-
