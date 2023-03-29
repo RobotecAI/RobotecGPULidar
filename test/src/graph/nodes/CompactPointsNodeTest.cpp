bb860f6b2117a803b818f85977ad6b3b9b1a9235
nebraszka <maja.nagarnowicz@robotec.ai>
2023-03-29 19:14:59 +0200

    Add surface tests of node formatting points and node compacting points

---
+++ test/src/graph/nodes/CompactPointsNodeTest.cpp
@@ -0,0 +1,20 @@
+#include <graph/Node.hpp>
+#include <gtest/gtest.h>
+#include <utils.hpp>
+
+using namespace testing;
+
+class CompactPointsNodeTest : public RGLAutoCleanupTest{ };
+
+TEST_F(CompactPointsNodeTest, invalid_arguments)
+{
+    EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_compact(nullptr), "node != nullptr");
+}
+
+TEST_F(CompactPointsNodeTest, valid_arguments)
+{
+    rgl_node_t compactNode = nullptr;
+
+    ASSERT_RGL_SUCCESS(rgl_node_points_compact(&compactNode));
+    EXPECT_THAT(compactNode, NotNull());
+}
