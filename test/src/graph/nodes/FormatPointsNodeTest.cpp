1a8cf8eba64e8cfd7acce9cb07a5ccdaeff5dd88
nebraszka <maja.nagarnowicz@robotec.ai>
2023-03-29 19:14:59 +0200

    Add surface tests of node formatting points and node compacting points

---
+++ test/src/graph/nodes/FormatPointsNodeTest.cpp
@@ -0,0 +1,29 @@
+#include <graph/Node.hpp>
+#include <gtest/gtest.h>
+#include <utils.hpp>
+
+using namespace testing;
+
+class FormatPointsNodeTests : public RGLTest{ };
+
+TEST_F(FormatPointsNodeTests, invalid_arguments)
+{
+    rgl_node_t formatNode = nullptr;
+	rgl_field_t field = RGL_FIELD_INTENSITY_F32;
+
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_format(nullptr, nullptr, 0), "node != nullptr");
+
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_format(&formatNode, nullptr, 0), "fields != nullptr");
+
+	formatNode = nullptr;
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_points_format(&formatNode, &field, 0), "field_count > 0");
+}
+
+TEST_F(FormatPointsNodeTests, valid_arguments)
+{
+	rgl_node_t formatNode = nullptr;
+	rgl_field_t field = RGL_FIELD_INTENSITY_F32;
+
+	EXPECT_RGL_SUCCESS(rgl_node_points_format(&formatNode, &field, 1));
+ 	ASSERT_THAT(formatNode, NotNull());
+}
