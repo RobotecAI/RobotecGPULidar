9feaa8958171005b6a869a12b20383ad07f4c74b
PiotrMrozik <piter012@vp.pl>
2023-05-10 15:46:01 +0200

    Add placeholder for basic texture test

--- test/src/texture.cpp
+++ test/src/texture.cpp
@@ -1,5 +1,24 @@
-//
-// Created by piotr on 10/05/23.
-//
+#include <gtest/gtest.h>
+#include "utils.hpp"
 
-#include "texture.h"
+
+class TextureTest : public RGLTest {
+
+protected:
+	uint32_t* generateTexture(int size)
+		{
+			uint32_t* pixels = new uint32_t[size * size];
+			for (int i = 0; i < size * size; i++) {
+				pixels[i] = 0xffffffff;
+			}
+			return pixels;
+		}
+};
+
+TEST_F(TextureTest, rgl_texture_create)
+{
+	auto textureData = generateTexture(256);
+	rgl_texture_create(&textureData, 256, 256);
+
+
+}
