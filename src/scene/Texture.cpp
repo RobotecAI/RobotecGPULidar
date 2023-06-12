7e863021bb38adb8429e59b297ad806ea618a75a
PiotrMrozik <piter012@vp.pl>
2023-06-12 11:20:18 +0200

    Zero pointer and change date

--- src/scene/Texture.cpp
+++ src/scene/Texture.cpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Robotec.AI
+// Copyright 2023 Robotec.AI
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -78,5 +78,6 @@ void Texture::cleanup()
 	cudaDestroyTextureObject(dTextureObject);
 	cudaFreeArray(dPixelArray);
 
+	dTextureObject = 0;
 	dPixelArray = nullptr;
 }
