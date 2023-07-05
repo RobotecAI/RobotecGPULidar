72fb12c1fa913cc0292bebfe5b0f42867a390c00
Mateusz Szczygielski <112629916+msz-rai@users.noreply.github.com>
2023-07-05 10:58:21 +0100

    Fix getting optional scene time (#162)

--- src/graph/RaytraceNode.cpp
+++ src/graph/RaytraceNode.cpp
@@ -64,7 +64,7 @@ void RaytraceNode::schedule(cudaStream_t stream)
 		.ringIds = ringIds.has_value() ? (*ringIds)->getDevicePtr() : nullptr,
 		.ringIdsCount = ringIds.has_value() ? (*ringIds)->getCount() : 0,
 		.scene = sceneAS,
-		.sceneTime = scene->getTime()->asSeconds(),
+		.sceneTime = scene->getTime().has_value() ? scene->getTime()->asSeconds() : 0,
 		.xyz = getPtrTo<XYZ_F32>(),
 		.isHit = getPtrTo<IS_HIT_I32>(),
 		.rayIdx = getPtrTo<RAY_IDX_U32>(),
