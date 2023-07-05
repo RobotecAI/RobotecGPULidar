5b5de4a15c332e413954be548d67276df3575520
nebraszka <majusxp@gmail.com>
2023-07-05 14:15:27 +0200

    Finished version with points generator

--- test/include/PointsTestHelper.hpp
+++ test/include/PointsTestHelper.hpp
@@ -1,111 +1,16 @@
-#include <random>
-#include <utils.hpp>
-#include <stdlib.h>
+#include <PointsGenerator.hpp>
 
-static std::random_device randomDevice;
-static auto randomSeed = randomDevice();
-static std::mt19937 randomGenerator { randomSeed };
-
-struct RGLPointsNodeTestHelper {
-protected:
+template <typename PointType>
+struct RGLTestUsePointsNodeHelper {
     rgl_node_t usePointsNode = nullptr;
+    std::unique_ptr<RGLTestPointsGeneratorBase<PointType>> generator;
+    int randomNonHitCount = 0;
 
-    std::vector<rgl_field_t> pointFields = {
-        XYZ_F32,
-        IS_HIT_I32,
-        INTENSITY_F32
-    };
+    void prepareUsePointsNode(int pointsCount, HitPointDensity hitPointDensity = HitPointDensity::NONE) { }
 
-    std::vector<rgl_field_t> pointFieldsWithoutIsHit = {
-        XYZ_F32,
-        INTENSITY_F32
-    };
-
-    struct TestPoint {
-        Field<XYZ_F32>::type xyz;
-        Field<IS_HIT_I32>::type isHit;
-        Field<INTENSITY_F32>::type intensity;
-    };
-
-    std::vector<TestPoint> inPoints;
-
-    struct TestPointWithoutIsHit {
-        Field<XYZ_F32>::type xyz;
-        Field<INTENSITY_F32>::type intensity;
-    };
-
-    enum class HitPointDensity {
-        HALF_HIT = 0,
-        ALL_NON_HIT,
-        ALL_HIT,
-        RANDOM
-    };
-
-    int32_t randomNonHitCount = 0;
-
-    void createTestUsePointsNode(
-        int pointsCount,
-        rgl_mat3x4f transform = identityTestTransform,
-        HitPointDensity hitPointDensity = HitPointDensity::HALF_HIT)
+    rgl_node_t simulateEmptyPointCloudOutputNode()
     {
-        inPoints = generateTestPoints(pointsCount, transform, hitPointDensity);
-
-        EXPECT_RGL_SUCCESS(rgl_node_points_from_array(&usePointsNode, inPoints.data(), inPoints.size(), pointFields.data(), pointFields.size()));
-        ASSERT_THAT(usePointsNode, testing::NotNull());
-    }
-
-    std::vector<TestPoint> generateTestPoints(
-        int count,
-        rgl_mat3x4f transform = identityTestTransform,
-        HitPointDensity hitPointDensity = HitPointDensity::HALF_HIT)
-    {
-        randomNonHitCount = 0;
-        std::vector<TestPoint> points;
-
-        for (int i = 0; i < count; ++i) {
-            auto currentPoint = TestPoint {
-                .xyz = { i, i + 1, i + 2 },
-                .isHit = determineIsHit(i, hitPointDensity),
-                .intensity = 100
-            };
-            currentPoint.xyz = Mat3x4f::fromRGL(transform) * currentPoint.xyz;
-            points.emplace_back(currentPoint);
-        }
-        return points;
-    }
-
-    // TODO(nebraszka): To be changed so as not to duplicate the code
-    std::vector<TestPointWithoutIsHit> generateTestPointsWithoutIsHit(
-        int count,
-        rgl_mat3x4f transform = identityTestTransform)
-    {
-        std::vector<TestPointWithoutIsHit> points;
-
-        for (int i = 0; i < count; ++i) {
-            auto currentPoint = TestPointWithoutIsHit {
-                .xyz = { i, i + 1, i + 2 },
-                .intensity = 100
-            };
-            currentPoint.xyz = Mat3x4f::fromRGL(transform) * currentPoint.xyz;
-            points.emplace_back(currentPoint);
-        }
-        return points;
-    }
-
-    std::vector<TestPoint> separateHitPoints()
-    {
-        std::vector<TestPoint> hitPoints;
-        for (auto point : inPoints) {
-            if (point.isHit == 1) {
-                hitPoints.push_back(point);
-            }
-        }
-        return hitPoints;
-    }
-
-    rgl_node_t simulateEmptyPointCloudInputNode()
-    {
-        createTestUsePointsNode(1, identityTestTransform, HitPointDensity::ALL_NON_HIT);
+        prepareUsePointsNode(1, HitPointDensity::ALL_NON_HIT);
         rgl_node_t compactNode = nullptr;
         EXPECT_RGL_SUCCESS(rgl_node_points_compact(&compactNode));
         EXPECT_RGL_SUCCESS(rgl_graph_node_add_child(usePointsNode, compactNode));
@@ -113,25 +18,95 @@ protected:
         return compactNode;
     }
 
-private:
-    int32_t determineIsHit(int index, HitPointDensity hitPointDensity)
+    std::vector<PointType> getResults(rgl_node_t dataNode)
     {
-        switch (hitPointDensity) {
-        case HitPointDensity::HALF_HIT:
-            return index % 2;
-        case HitPointDensity::ALL_NON_HIT:
-            return 0;
-        case HitPointDensity::ALL_HIT:
-            return 1;
-        case HitPointDensity::RANDOM: {
-            int isHit = randomGenerator() % 2;
-            if (!isHit) {
-                randomNonHitCount++;
+        EXPECT_RGL_SUCCESS(rgl_graph_run(dataNode));
+
+        int32_t outCount = 0, outSizeOf = 0;
+
+        std::vector<PointType> resultPoints;
+        auto pointFields = generator->getPointFields();
+
+        for (auto field : pointFields) {
+            EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(dataNode, field, &outCount, &outSizeOf));
+            EXPECT_EQ(outSizeOf, getFieldSize(field));
+
+            switch (field) {
+            case XYZ_F32: {
+                std::vector<::Field<XYZ_F32>::type> outData(outCount);
+                if (outCount != 0) {
+                    EXPECT_RGL_SUCCESS(rgl_graph_get_result_data(dataNode, field, outData.data()));
+                    resultPoints.resize(outCount);
+                    for (int i = 0; i < outCount; ++i) {
+                        resultPoints[i].xyz = outData[i];
+                    }
+                }
+                break;
             }
-            return isHit;
+            case IS_HIT_I32: {
+                std::vector<::Field<IS_HIT_I32>::type> outData(outCount);
+                if (outCount != 0) {
+                    EXPECT_RGL_SUCCESS(rgl_graph_get_result_data(dataNode, field, outData.data()));
+                    for (int i = 0; i < outCount; ++i) {
+                        resultPoints[i].isHit = outData[i];
+                    }
+                }
+                break;
+            }
+            case INTENSITY_F32: {
+                std::vector<::Field<INTENSITY_F32>::type> outData(outCount);
+                if (outCount != 0) {
+                    EXPECT_RGL_SUCCESS(rgl_graph_get_result_data(dataNode, field, outData.data()));
+                    for (int i = 0; i < outCount; ++i) {
+                        resultPoints[i].intensity = outData[i];
+                    }
+                }
+                break;
+            }
+            }
         }
-        default:
-            return 0;
+
+        return resultPoints;
+    }
+
+    void verifyResults(const std::vector<PointType>& resultPoints, const std::vector<PointType>& expectedPoints)
+    {
+        EXPECT_EQ(resultPoints.size(), expectedPoints.size());
+        auto pointFields = generator->getPointFields();
+
+        for (size_t i = 0; i < resultPoints.size(); ++i) {
+            EXPECT_EQ(expectedPoints.at(i), resultPoints.at(i));
         }
     }
+
+    void createUsePointsNode(const auto& generator)
+    {
+        ASSERT_RGL_SUCCESS(rgl_node_points_from_array(
+            &usePointsNode,
+            generator->getPointsData(),
+            generator->getPointsSize(),
+            generator->getPointFields().data(),
+            generator->getPointFields().size()));
+
+        ASSERT_THAT(usePointsNode, testing::NotNull());
+    }
 };
+
+template <>
+inline void RGLTestUsePointsNodeHelper<TestPoint>::prepareUsePointsNode(int pointsCount, HitPointDensity hitPointDensity)
+{
+    auto pointGenerator = std::make_unique<RGLTestPointsGenerator>();
+    pointGenerator->generateTestPoints(pointsCount);
+    createUsePointsNode(pointGenerator.get());
+    generator = std::move(pointGenerator);
+}
+
+template <>
+inline void RGLTestUsePointsNodeHelper<TestPointIsHit>::prepareUsePointsNode(int pointsCount, HitPointDensity hitPointDensity)
+{
+    auto pointGenerator = std::make_unique<RGLTestPointsIsHitGenerator>(hitPointDensity);
+    pointGenerator->generateTestPoints(pointsCount);
+    randomNonHitCount = pointGenerator->getRandomNonHitCount();
+    createUsePointsNode(pointGenerator.get());
+    generator = std::move(pointGenerator);
+}
