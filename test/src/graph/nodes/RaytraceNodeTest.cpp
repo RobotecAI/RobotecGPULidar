e15081a02dfb10dc31b80f4545c6c38fbc47ff56
nebraszka <majusxp@gmail.com>
2023-07-18 13:17:29 +0200

    Add test with empty scene

--- test/src/graph/nodes/RaytraceNodeTest.cpp
+++ test/src/graph/nodes/RaytraceNodeTest.cpp
@@ -1,8 +1,11 @@
+#include <scenes.hpp>
 #include <utils.hpp>
 
 class RaytraceNodeTest : public RGLTest {
 protected:
     rgl_node_t raytraceNode;
+
+    float raytraceDepth;
 
     RaytraceNodeTest()
     {
@@ -27,4 +30,34 @@ TEST_F(RaytraceNodeTest, valid_arguments)
 
     // If (*raytraceNode) != nullptr
     EXPECT_RGL_SUCCESS(rgl_node_raytrace(&raytraceNode, nullptr, 1.0f));
+}
+
+TEST_F(RaytraceNodeTest, empty_scene)
+{
+    const rgl_mat3x4f identity = Mat3x4f::identity().toRGL();
+
+    rgl_node_t raysFromMat = nullptr;
+    ASSERT_RGL_SUCCESS(rgl_node_rays_from_mat3x4f(&raysFromMat, &identity, 1));
+
+    raytraceDepth = 10.0f;
+    ASSERT_RGL_SUCCESS(rgl_node_raytrace(&raytraceNode, nullptr, raytraceDepth));
+
+    ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(raysFromMat, raytraceNode));
+
+    ASSERT_RGL_SUCCESS(rgl_graph_run(raytraceNode));
+
+    // The test fails. Important note:
+    // When trying to take out the size of the results, an error is returned
+    // Error message: Recoverable error (code=1): requested to reserve 0 elements
+    int32_t hitpointCount, pointSize;
+    EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(raytraceNode, RGL_FIELD_XYZ_F32, &hitpointCount, &pointSize));
+
+    // Also returned is an error from the rgl_cleanup()
+    // Error message: Recoverable error (code=1): requested to reserve 0 elements
+
+    // Potential solutions: 
+    // warning that the scene is empty and performing a raytracing 
+    // or error with the corresponding message that the scene is empty
+
+    // rgl_cleanup() should also return a warning or error with a better specified comment
 }
