76ffb7dc71e881067ee072c282d49e92fa0a5393
nebraszka <majusxp@gmail.com>
2023-07-20 10:34:37 +0200

    Add tests for raytrace depth

--- test/src/graph/nodes/RaytraceNodeTest.cpp
+++ test/src/graph/nodes/RaytraceNodeTest.cpp
@@ -1,16 +1,48 @@
 #include <scenes.hpp>
 #include <utils.hpp>
+#include <RGLFields.hpp>
+
+static constexpr float CUBE_HALF_EDGE = 1.0;
 
 class RaytraceNodeTest : public RGLTest {
 protected:
-    rgl_node_t raytraceNode;
-
-    float raytraceDepth;
-
     RaytraceNodeTest()
     {
         raytraceNode = nullptr;
+        raysFromMatNode = nullptr;
     }
+
+    void prepareScene()
+    {
+        const Mat3x4f cubeTf = Mat3x4f::translation(0, 0, cubePosition.z());
+        spawnCubeOnScene(nullptr, cubeTf);
+    }
+
+    void prepareGraph()
+    {
+        ASSERT_RGL_SUCCESS(rgl_node_rays_from_mat3x4f(&raysFromMatNode, &identityTf, 1)); // TODO change hardcoded size
+        ASSERT_RGL_SUCCESS(rgl_node_raytrace(&raytraceNode, nullptr, raytraceDepth));
+        ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(raysFromMatNode, raytraceNode));
+    }
+
+    void getResults()
+    {
+        ASSERT_RGL_SUCCESS(rgl_graph_run(raytraceNode));
+
+        ASSERT_RGL_SUCCESS(rgl_graph_get_result_size(raytraceNode, RGL_FIELD_XYZ_F32, &pointCount, &pointSize));
+        ASSERT_EQ(pointCount, 1);
+        ASSERT_EQ(pointSize, sizeof(Vec3f));
+
+        ASSERT_RGL_SUCCESS(rgl_graph_get_result_data(raytraceNode, RGL_FIELD_XYZ_F32, &point));
+    }
+
+    Vec3f cubePosition = Vec3f(0, 0, 55.5f);
+    rgl_mat3x4f identityTf = Mat3x4f::identity().toRGL();
+
+    rgl_node_t raytraceNode, raysFromMatNode;
+    float raytraceDepth;
+    int32_t pointCount, pointSize;
+    Vec3f point;
 };
 
 TEST_F(RaytraceNodeTest, invalid_argument_node)
@@ -34,30 +66,59 @@ TEST_F(RaytraceNodeTest, valid_arguments)
 
 TEST_F(RaytraceNodeTest, empty_scene)
 {
-    const rgl_mat3x4f identity = Mat3x4f::identity().toRGL();
-
-    rgl_node_t raysFromMat = nullptr;
-    ASSERT_RGL_SUCCESS(rgl_node_rays_from_mat3x4f(&raysFromMat, &identity, 1));
-
+    /* 
     raytraceDepth = 10.0f;
-    ASSERT_RGL_SUCCESS(rgl_node_raytrace(&raytraceNode, nullptr, raytraceDepth));
+    prepareGraph();
 
-    ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(raysFromMat, raytraceNode));
-
-    ASSERT_RGL_SUCCESS(rgl_graph_run(raytraceNode));
-
-    // The test fails. Important note:
     // When trying to take out the size of the results, an error is returned
     // Error message: Recoverable error (code=1): requested to reserve 0 elements
-    int32_t hitpointCount, pointSize;
-    EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(raytraceNode, RGL_FIELD_XYZ_F32, &hitpointCount, &pointSize));
+    EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(raytraceNode, RGL_FIELD_XYZ_F32, &pointCount, &pointSize));
 
     // Also returned is an error from the rgl_cleanup()
     // Error message: Recoverable error (code=1): requested to reserve 0 elements
+    */
+
+    // The test fails. It is commented because failure of rgl_cleanup affects other tests
+    FAIL();
 
     // Potential solutions: 
     // warning that the scene is empty and performing a raytracing 
     // or error with the corresponding message that the scene is empty
-
     // rgl_cleanup() should also return a warning or error with a better specified comment
+}
+
+TEST_F(RaytraceNodeTest, raytrace_misses_object_when_depth_is_shorter_than_distance)
+{
+    raytraceDepth = cubePosition.z() - CUBE_HALF_EDGE - 1.0f;
+    prepareScene();
+    prepareGraph();
+    getResults();
+
+    EXPECT_EQ(point.x(), NON_HIT_VALUE);
+    EXPECT_EQ(point.y(), NON_HIT_VALUE);
+    EXPECT_EQ(point.z(), NON_HIT_VALUE);
+}
+
+TEST_F(RaytraceNodeTest, raytrace_hits_object_when_depth_equals_distance)
+{
+    raytraceDepth = cubePosition.z() - CUBE_HALF_EDGE;
+    prepareScene();
+    prepareGraph();
+    getResults();
+
+    EXPECT_EQ(point.x(), cubePosition.x());
+    EXPECT_EQ(point.y(), cubePosition.y());
+    EXPECT_EQ(point.z(), cubePosition.z() - CUBE_HALF_EDGE);
+}
+
+TEST_F(RaytraceNodeTest, raytrace_hits_object_when_depth_is_longer_than_distance)
+{
+    raytraceDepth = cubePosition.z() - CUBE_HALF_EDGE + 1.0f;
+    prepareScene();
+    prepareGraph();
+    getResults();
+
+    EXPECT_EQ(point.x(), cubePosition.x());
+    EXPECT_EQ(point.y(), cubePosition.y());
+    EXPECT_EQ(point.z(), cubePosition.z() - CUBE_HALF_EDGE);
 }
