0b6d0c191838256af9ad292288870ea8645045ea
Piotr Rybicki <piotr.rybicki@robotec.ai>
2023-08-11 09:39:09 +0200

    Generalize Array typing tests

--- test/src/memory/arrayTest.cpp
+++ test/src/memory/arrayTest.cpp
@@ -3,32 +3,91 @@
 
 #include <memory/InvalidArrayCast.hpp>
 #include <memory/HostPinnedArray.hpp>
+#include <memory/DeviceSyncArray.hpp>
 
 using namespace ::testing;
 
-TEST(Array, Typing)
+/*
+ * TEST PURPOSE:
+ * Check that *Array type casting works as expected:
+ * - Throws on bad casts: mismatched data type, mismatched subclass (memory kind)
+ * - Works for correct casts: allows to convert from a concrete type to IAnyArray and back.
+ */
+
+template<typename T, typename U, template<typename> typename V, template<typename> typename  W>
+struct ArrayTypes
 {
-	using Type = int;
-	using WrongType = float;
+	using Data = T;
 
-	// Create Array and its untyped handle.
-	HostPinnedArray<Type>::Ptr arrayOriginal = HostPinnedArray<Type>::create();
-	IAnyArray<MemoryKind::HostPinned>::ConstPtr arrayAny = arrayOriginal->asAnyArray();
-	arrayOriginal->append(static_cast<Type>(42));
+	using WrongData = U;
 
-	// Attempt casting to a wrong Array type, should throw
-	// Lambda because comma in explicit template parameter list fools GTEST macros
-	auto cast = [&]() { arrayOriginal->asTypedArray<WrongType, HostPinnedArray>(); };
-	EXPECT_THROW(cast(), InvalidArrayCast);
+	template<typename TT>
+	using Subclass = V<TT>;
 
-	// Casting to wrong memory kind is a compile error!
-	// auto shouldNotCompile = arrayAny->asTypedArray<Type, DeviceSyncArray>();
+	template<typename TT>
+	using WrongSubclass = W<TT>;
+};
 
-	// Check casting to correct type works
-	HostArray<MemoryKind::HostPinned, Type>::ConstPtr arrayOfTypeRecovered = arrayAny->asTypedArray<Type, HostPinnedArray>();
+using CastingCombinations = ::testing::Types<
+		ArrayTypes<int, float, DeviceAsyncArray, DeviceSyncArray>,
+		ArrayTypes<int, float, DeviceAsyncArray, HostPageableArray>,
+		ArrayTypes<int, float, DeviceAsyncArray, HostPinnedArray>,
+
+		ArrayTypes<int, float, DeviceSyncArray, DeviceAsyncArray>,
+		ArrayTypes<int, float, DeviceSyncArray, HostPageableArray>,
+		ArrayTypes<int, float, DeviceSyncArray, HostPinnedArray>,
+
+		ArrayTypes<int, float, HostPageableArray, DeviceAsyncArray>,
+		ArrayTypes<int, float, HostPageableArray, DeviceSyncArray>,
+		ArrayTypes<int, float, HostPageableArray, HostPinnedArray>,
+
+		ArrayTypes<int, float, HostPinnedArray, DeviceAsyncArray>,
+		ArrayTypes<int, float, HostPinnedArray, DeviceSyncArray>,
+		ArrayTypes<int, float, HostPinnedArray, HostPageableArray>
+>;
+
+template <typename T>
+class ArrayTyping : public ::testing::Test {
+protected:
+	using Data = typename T::Data;
+	using WrongData = typename T::WrongData;
+
+	template<typename TT>
+	using Subclass = typename T::template Subclass<TT>;
+
+	template<typename TT>
+	using WrongSubclass = typename T::template WrongSubclass<TT>;
+};
+TYPED_TEST_SUITE(ArrayTyping, CastingCombinations);
+
+TYPED_TEST(ArrayTyping, Typing) {
+	// Here, you can use Type, WrongType, RealSubclass, and WrongSubclass as if they were type aliases
+	// For example:
+	using SubclassType = typename TypeParam::template Subclass<typename TypeParam::Data>;
+	typename SubclassType::Ptr arrayOriginal = SubclassType::create();
+	IAnyArray::Ptr arrayAny = arrayOriginal->asAnyArray();
+	arrayAny->resize(1, true, false);
+
+	{
+		// Attempt casting to a wrong Array data type, should throw
+		// Lambda because comma in explicit template parameter list fools GTEST macros
+		auto cast = [&]() { arrayOriginal->template asTypedArray<typename TypeParam::WrongData, TypeParam::template Subclass>(); };
+		EXPECT_THROW(cast(), InvalidArrayCast);
+	}
+
+	{
+		// Attempt casting to a wrong Array subclass, should throw
+		// Lambda because comma in explicit template parameter list fools GTEST macros
+		auto cast = [&]() { arrayOriginal->template asTypedArray<typename TypeParam::Data, TypeParam::template WrongSubclass>(); };
+		EXPECT_THROW(cast(), InvalidArrayCast);
+	}
+	// Continue your tests as usual
+
+	typename SubclassType::ConstPtr arrayOfTypeRecovered = arrayAny->asTypedArray<typename TypeParam::Data, TypeParam::template Subclass>();
 	EXPECT_THAT(arrayOfTypeRecovered, NotNull());
 	EXPECT_THAT(arrayOfTypeRecovered, Eq(arrayOriginal));
-	EXPECT_EQ(arrayOfTypeRecovered->at(0), static_cast<Type>(42));
+	EXPECT_EQ(arrayOfTypeRecovered->getCount(), 1);
+	EXPECT_EQ(arrayOfTypeRecovered->getSizeOf(), sizeof(typename TypeParam::Data));
 }
 
 struct ArrayOps : public ::testing::Test
