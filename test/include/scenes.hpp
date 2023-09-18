62af33a03df384b7404d18596b977a873ab88935
nebraszka <majusxp@gmail.com>
2023-09-18 19:45:14 +0200

    Models and scenes code cleanup

--- test/include/scenes.hpp
+++ test/include/scenes.hpp
@@ -1,15 +1,12 @@
 #pragma once
 
-#include <math/Mat3x4f.hpp>
-#include <models.hpp>
-#include <gtest/gtest.h>
 #include <utils.hpp>
 
 static rgl_mesh_t makeCubeMesh()
 {
 	rgl_mesh_t mesh = nullptr;
 	EXPECT_RGL_SUCCESS(rgl_mesh_create(&mesh, cubeVertices, ARRAY_SIZE(cubeVertices), cubeIndices, ARRAY_SIZE(cubeIndices)));
-	EXPECT_THAT(mesh, ::testing::NotNull());
+	EXPECT_THAT(mesh, testing::NotNull());
 	return mesh;
 }
 
@@ -38,16 +35,14 @@ static inline void spawnCubeOnScene(rgl_scene_t scene,
 	}
 }
 
-static inline void setupBoxesAlongAxes(rgl_scene_t scene) {
-
+static inline void setupBoxesAlongAxes(rgl_scene_t scene)
+{
 	constexpr int BOX_COUNT = 10;
 	constexpr float scaleX = 1.0f;
 	constexpr float scaleY = 2.0f;
 	constexpr float scaleZ = 3.0f;
 
 	for (int i = 0; i < BOX_COUNT; ++i) {
-
-
 		spawnCubeOnScene(scene, Mat3x4f::TRS({(2 * scaleX + 2) * i, 0, 0}, {45, 0, 0}, {scaleX, 1, 1}));
 		spawnCubeOnScene(scene, Mat3x4f::TRS({0, (2 * scaleY + 2) * i, 0}, {0, 45, 0}, {1, scaleY, 1}));
 		spawnCubeOnScene(scene, Mat3x4f::TRS({0, 0, (2 * scaleZ + 2) * i}, {0, 0, 45}, {1, 1, scaleZ}));
