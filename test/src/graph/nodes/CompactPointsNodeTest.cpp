53c06401b74d1c478456235d213267a51e9cd4bf
nebraszka <majusxp@gmail.com>
2023-10-31 14:56:55 +0100

    Rebase fixes

--- test/src/graph/nodes/CompactPointsNodeTest.cpp
+++ test/src/graph/nodes/CompactPointsNodeTest.cpp
@@ -21,16 +21,16 @@ public:
 protected:
 	rgl_node_t compactPointsNode = nullptr;
 
-	std::vector<rgl_field_t> pointFields = {XYZ_F32, IS_HIT_I32};
+	std::vector<rgl_field_t> pointFields = {XYZ_VEC3_F32, IS_HIT_I32};
 
 	std::pair<std::unique_ptr<TestPointCloud>, rgl_node_t> createPointCloud(
-	    int pointsCount, const std::function<Field<XYZ_F32>::type(int)>& genCoords,
+	    int pointsCount, const std::function<Field<XYZ_VEC3_F32>::type(int)>& genCoords,
 	    const std::function<Field<IS_HIT_I32>::type(int)>& genIsHit)
 	{
 		std::unique_ptr<TestPointCloud> pointCloud = std::make_unique<TestPointCloud>(pointFields, pointsCount);
 		auto pointCoordValues = generateFieldValues(pointsCount, genCoords);
 		auto pointIsHitValues = generateFieldValues(pointsCount, genIsHit);
-		pointCloud->setFieldValues<XYZ_F32>(pointCoordValues);
+		pointCloud->setFieldValues<XYZ_VEC3_F32>(pointCoordValues);
 		pointCloud->setFieldValues<IS_HIT_I32>(pointIsHitValues);
 		return {std::move(pointCloud), pointCloud->createUsePointsNode()};
 	}
@@ -97,7 +97,7 @@ TEST_P(CompactPointsNodeTest, points_all_non_hit)
 	EXPECT_EQ(outPointCloud->getPointCount(), 0);
 
 	// Check if the contents of outData have changed (they should not have)
-	std::vector<::Field<XYZ_F32>::type> outData{
+	std::vector<::Field<XYZ_VEC3_F32>::type> outData{
 	    { 2.0f,  3.0f,  5.0f},
         { 7.0f, 11.0f, 13.0f},
         {17.0f, 19.0f, 23.0f}
@@ -105,12 +105,12 @@ TEST_P(CompactPointsNodeTest, points_all_non_hit)
 	auto outDataCopy = outData;
 
 	int32_t outCount, outSize;
-	ASSERT_RGL_SUCCESS(rgl_graph_get_result_size(compactPointsNode, RGL_FIELD_XYZ_F32, &outCount, &outSize));
-	ASSERT_EQ(outSize, sizeof(Field<XYZ_F32>::type));
+	ASSERT_RGL_SUCCESS(rgl_graph_get_result_size(compactPointsNode, RGL_FIELD_XYZ_VEC3_F32, &outCount, &outSize));
+	ASSERT_EQ(outSize, sizeof(Field<XYZ_VEC3_F32>::type));
 	ASSERT_EQ(outCount, 0);
 
 	// Verify that the data is unchanged; this is a sanity check to make sure that the data is not overwritten
-	ASSERT_RGL_SUCCESS(rgl_graph_get_result_data(compactPointsNode, RGL_FIELD_XYZ_F32, outData.data()));
+	ASSERT_RGL_SUCCESS(rgl_graph_get_result_data(compactPointsNode, RGL_FIELD_XYZ_VEC3_F32, outData.data()));
 	ASSERT_EQ(outData.size(), outDataCopy.size());
 	for (int i = 0; i < outData.size(); ++i) {
 		EXPECT_EQ(outData.at(i)[0], outDataCopy.at(i)[0]);
@@ -192,11 +192,11 @@ TEST_F(CompactPointsNodeTest, should_warn_when_empty_point_cloud)
 TEST_F(CompactPointsNodeTest, without_IS_HIT_field)
 {
 	int pointCount = 10;
-	std::vector<rgl_field_t> pointFields = {XYZ_F32};
+	std::vector<rgl_field_t> pointFields = {XYZ_VEC3_F32};
 
 	auto&& inPointCloud = std::make_unique<TestPointCloud>(pointFields, pointCount);
 	auto pointCoordValues = generateFieldValues(pointCount, genCoord);
-	inPointCloud->setFieldValues<XYZ_F32>(pointCoordValues);
+	inPointCloud->setFieldValues<XYZ_VEC3_F32>(pointCoordValues);
 
 	rgl_node_t usePointsNode = inPointCloud->createUsePointsNode();
 	ASSERT_THAT(usePointsNode, testing::NotNull());
