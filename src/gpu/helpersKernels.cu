2700ac9ce15d2bf9ad999c3d327b501dae22c084
PiotrMrozik <piter012@vp.pl>
2023-11-08 19:06:49 +0100

    migrate hernel helpers from other branch

---
+++ src/gpu/helpersKernels.cu
@@ -0,0 +1,34 @@
+// Copyright 2023 Robotec.AI
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cuda.h>
+#include <curand_kernel.h>
+#include <gpu/kernelUtils.hpp>
+
+#include <gpu/helpersKernels.hpp>
+
+// Philox algorithm chosen based on performance
+// https://stackoverflow.com/questions/18506697/curand-properties-of-generators
+__global__ void kSetupRandomNumberGenerator(size_t pointCount, unsigned int seed, curandStatePhilox4_32_10_t* states)
+{
+	LIMIT(pointCount);
+	/* Each thread gets same seed, a different sequence number, no offset */
+	curand_init(seed, tid, 0, &states[tid]);
+}
+
+void gpuSetupRandomNumberGenerator(cudaStream_t stream, size_t elementsCount, unsigned int seed,
+                                   curandStatePhilox4_32_10_t* outPHILOXStates)
+{
+	run(kSetupRandomNumberGenerator, stream, elementsCount, seed, outPHILOXStates);
+}
--- src/graph/GaussianNoiseAngularHitpointNode.cpp
+++ src/graph/GaussianNoiseAngularHitpointNode.cpp
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <graph/NodesCore.hpp>
+#include <gpu/helpersKernels.hpp>
 #include <gpu/gaussianNoiseKernels.hpp>
+#include <graph/NodesCore.hpp>
 
 void GaussianNoiseAngularHitpointNode::setParameters(float mean, float stDev, rgl_axis_t rotationAxis)
 {
@@ -52,7 +53,7 @@ void GaussianNoiseAngularHitpointNode::enqueueExecImpl()
 
 	if (randomizationStates->getCount() < pointCount) {
 		randomizationStates->resize(pointCount, false, false);
-		gpuSetupGaussianNoiseGenerator(getStreamHandle(), pointCount, randomDevice(), randomizationStates->getWritePtr());
+		gpuSetupRandomNumberGenerator(getStreamHandle(), pointCount, randomDevice(), randomizationStates->getWritePtr());
 	}
 
 	const auto* inXyzPtr = input->getFieldDataTyped<XYZ_VEC3_F32>()->asSubclass<DeviceAsyncArray>()->getReadPtr();
