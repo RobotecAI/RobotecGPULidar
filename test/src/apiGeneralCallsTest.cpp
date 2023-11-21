3c17961b6a9e395920cf9164b200715ef7813773
nebraszka <majusxp@gmail.com>
2023-11-21 13:45:11 +0100

    Create apiGeneralCallsTest

--- test/src/apiGeneralCallsTest.cpp
+++ test/src/apiGeneralCallsTest.cpp
@@ -6,14 +6,30 @@
 
 using namespace ::testing;
 
-class LoggingTests : public RGLTest
+class GeneralCallsTest : public RGLTest
 {
 protected:
 	std::string logFilePath{
 	    (std::filesystem::temp_directory_path() / std::filesystem::path("loggingTests").concat(".log")).string()};
 };
 
-TEST_F(LoggingTests, rgl_configure_logging)
+TEST_F(GeneralCallsTest, rgl_get_version_info)
+{
+	int major, minor, patch;
+
+	// Invalid args
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_get_version_info(nullptr, nullptr, nullptr), "out_major != nullptr");
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_get_version_info(&major, nullptr, nullptr), "out_minor != nullptr");
+	EXPECT_RGL_INVALID_ARGUMENT(rgl_get_version_info(&major, &minor, nullptr), "out_patch != nullptr");
+
+	// Valid args
+	ASSERT_RGL_SUCCESS(rgl_get_version_info(&major, &minor, &patch));
+	EXPECT_EQ(major, RGL_VERSION_MAJOR);
+	EXPECT_EQ(minor, RGL_VERSION_MINOR);
+	EXPECT_EQ(patch, RGL_VERSION_PATCH);
+}
+
+TEST_F(GeneralCallsTest, rgl_configure_logging)
 {
 	// Setup logging
 	ASSERT_THAT(logFilePath.c_str(), testing::NotNull());
