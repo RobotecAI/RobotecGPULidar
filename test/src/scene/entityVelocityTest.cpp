c824e1ff8338d42c9f08431ef581dd94ebfc764b
nebraszka <majusxp@gmail.com>
2024-01-03 11:46:11 +0100

    Work in progress

--- test/src/scene/entityVelocityTest.cpp
+++ test/src/scene/entityVelocityTest.cpp
@@ -18,6 +18,7 @@ struct TestScene
 	{
 		DynamicCube(std::function<void(DynamicCube&, float)> updateFn) : updateFn(updateFn)
 		{
+			//Nebraszka: dlaczego za każdym razem tworzymy nowy mesh, zamiast użyć jednego i tworzyć z niego kolejne entities?
 			EXPECT_RGL_SUCCESS(
 			    rgl_mesh_create(&mesh, cubeVertices, ARRAY_SIZE(cubeVertices), cubeIndices, ARRAY_SIZE(cubeIndices)));
 			EXPECT_RGL_SUCCESS(rgl_entity_create(&entity, nullptr, mesh));
@@ -135,15 +136,17 @@ struct TestScene
  * *) outputs = {RGL_FIELD_ABSOLUTE_VELOCITY_VEC3_F32, RGL_FIELD_RELATIVE_VELOCITY_VEC3_F32, RGL_FIELD_RADIAL_SPEED_F32}
  */
 #if RGL_BUILD_ROS2_EXTENSION
+
 #include <rgl/api/extensions/ros2.h>
 #include <graph/NodesRos2.hpp>
+
 TEST(EntityVelocity, Interactive)
 {
-	GTEST_SKIP(); // Comment to run the test
+	//	GTEST_SKIP(); // Comment to run the test
 	rgl_node_t rays = nullptr, rayTransform = nullptr, raytrace = nullptr, compact = nullptr, format = nullptr,
 	           publish = nullptr, markersRelVel = nullptr, markersAbsVel = nullptr, transformPoints = nullptr;
 	std::vector<rgl_mat3x4f> raysTf = makeLidar3dRays(360.0f, 180.0f);
-	rgl_mat3x4f lidarPoseRGL = Mat3x4f::identity().toRGL(), lidarIndicatorPoseRGL;
+	rgl_mat3x4f lidarPoseRGL = Mat3x4f::identity().toRGL(), lidarIndicatorPoseRGL; //Nebraszka: nie rozumiem tego
 
 	// Published fields
 	std::vector<rgl_field_t> fields = {RGL_FIELD_XYZ_VEC3_F32, RGL_FIELD_ABSOLUTE_VELOCITY_VEC3_F32,
@@ -208,4 +211,193 @@ TEST(EntityVelocity, Interactive)
 		frameId += 1;
 	}
 }
+
 #endif
+
+class EntityVelocityUnitTests : public RGLTest
+{};
+
+TEST_F(EntityVelocityUnitTests, StaticLidarStaticEntity)
+{
+	rgl_node_t rays = nullptr, raytrace = nullptr, compact = nullptr, format = nullptr;
+	std::vector<rgl_mat3x4f> raysTf = {Mat3x4f::identity().toRGL()};
+	std::vector<rgl_field_t> velocityFields = {RGL_FIELD_ABSOLUTE_VELOCITY_VEC3_F32, RGL_FIELD_RELATIVE_VELOCITY_VEC3_F32,
+	                                           RGL_FIELD_RADIAL_SPEED_F32};
+	auto cubePosition = Vec3f{0, 0, 5};
+	spawnCubeOnScene(Mat3x4f::translation(cubePosition));
+
+	ASSERT_RGL_SUCCESS(rgl_node_rays_from_mat3x4f(&rays, raysTf.data(), raysTf.size()));
+	ASSERT_RGL_SUCCESS(rgl_node_raytrace(&raytrace, nullptr));
+	ASSERT_RGL_SUCCESS(rgl_node_points_compact(&compact));
+	ASSERT_RGL_SUCCESS(rgl_node_points_format(&format, velocityFields.data(), velocityFields.size()));
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(rays, raytrace));
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(raytrace, compact));
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(compact, format));
+
+	uint64_t currentTime = 0;
+	ASSERT_RGL_SUCCESS(rgl_scene_set_time(nullptr, currentTime));
+	ASSERT_RGL_SUCCESS(rgl_graph_run(raytrace));
+
+	currentTime += 1;
+	ASSERT_RGL_SUCCESS(rgl_scene_set_time(nullptr, currentTime));
+	ASSERT_RGL_SUCCESS(rgl_graph_run(raytrace));
+
+	int32_t outCount, outSize;
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(format, RGL_FIELD_ABSOLUTE_VELOCITY_VEC3_F32, &outCount, &outSize));
+	EXPECT_EQ(outCount, raysTf.size());
+	EXPECT_EQ(outSize, sizeof(Vec3f));
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(format, RGL_FIELD_RELATIVE_VELOCITY_VEC3_F32, &outCount, &outSize));
+	EXPECT_EQ(outCount, raysTf.size());
+	EXPECT_EQ(outSize, sizeof(Vec3f));
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(format, RGL_FIELD_RADIAL_SPEED_F32, &outCount, &outSize));
+	EXPECT_EQ(outCount, raysTf.size());
+	EXPECT_EQ(outSize, sizeof(float));
+
+	Vec3f absVelocity, relVelocity;
+	float radVelocity;
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_data(format, RGL_FIELD_ABSOLUTE_VELOCITY_VEC3_F32, &absVelocity));
+	EXPECT_EQ(absVelocity.x(), 0.0f);
+	EXPECT_EQ(absVelocity.y(), 0.0f);
+	EXPECT_EQ(absVelocity.z(), 0.0f);
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_data(format, RGL_FIELD_RELATIVE_VELOCITY_VEC3_F32, &relVelocity));
+	EXPECT_EQ(relVelocity.x(), 0.0f);
+	EXPECT_EQ(relVelocity.y(), 0.0f);
+	EXPECT_EQ(relVelocity.z(), 0.0f);
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_data(format, RGL_FIELD_RADIAL_SPEED_F32, &radVelocity));
+	EXPECT_EQ(radVelocity, 0.0f);
+}
+
+TEST_F(EntityVelocityUnitTests, StationaryLidarTranslatingEntity)
+{
+	rgl_node_t rays = nullptr, raytrace = nullptr, compact = nullptr, format = nullptr;
+	std::vector<rgl_mat3x4f> raysTf = {Mat3x4f::identity().toRGL()};
+	std::vector<rgl_field_t> velocityFields = {RGL_FIELD_ABSOLUTE_VELOCITY_VEC3_F32, RGL_FIELD_RELATIVE_VELOCITY_VEC3_F32,
+	                                           RGL_FIELD_RADIAL_SPEED_F32};
+	auto cubePosition = Vec3f{0, 0, 5};
+	auto cubeVelocity = Vec3f{0, 0, 2};
+	rgl_entity_t cubeEntity = spawnCubeOnScene(Mat3x4f::translation(cubePosition));
+
+	ASSERT_RGL_SUCCESS(rgl_node_rays_from_mat3x4f(&rays, raysTf.data(), raysTf.size()));
+	ASSERT_RGL_SUCCESS(rgl_node_raytrace(&raytrace, nullptr));
+	ASSERT_RGL_SUCCESS(rgl_node_points_compact(&compact));
+	ASSERT_RGL_SUCCESS(rgl_node_points_format(&format, velocityFields.data(), velocityFields.size()));
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(rays, raytrace));
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(raytrace, compact));
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(compact, format));
+
+	uint64_t deltaTime = 10 * 1000 * 1000;
+	uint64_t currentTime = 0;
+	ASSERT_RGL_SUCCESS(rgl_scene_set_time(nullptr, currentTime));
+	ASSERT_RGL_SUCCESS(rgl_graph_run(raytrace));
+
+	for (int i = 0; i < 2; i++) {
+		currentTime += deltaTime;
+		ASSERT_RGL_SUCCESS(rgl_scene_set_time(nullptr, currentTime));
+		Vec3f cubeTranslVec = cubeVelocity * Vec3f{deltaTime, deltaTime, deltaTime};
+		rgl_mat3x4f newPose = Mat3x4f::translation(cubePosition + cubeTranslVec).toRGL();
+		ASSERT_RGL_SUCCESS(rgl_entity_set_pose(cubeEntity, &newPose));
+		ASSERT_RGL_SUCCESS(rgl_graph_run(raytrace));
+		std::this_thread::sleep_for(10ms);
+	}
+
+	int32_t outCount, outSize;
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(format, RGL_FIELD_ABSOLUTE_VELOCITY_VEC3_F32, &outCount, &outSize));
+	EXPECT_EQ(outCount, raysTf.size());
+	EXPECT_EQ(outSize, sizeof(Vec3f));
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(format, RGL_FIELD_RELATIVE_VELOCITY_VEC3_F32, &outCount, &outSize));
+	EXPECT_EQ(outCount, raysTf.size());
+	EXPECT_EQ(outSize, sizeof(Vec3f));
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(format, RGL_FIELD_RADIAL_SPEED_F32, &outCount, &outSize));
+	EXPECT_EQ(outCount, raysTf.size());
+	EXPECT_EQ(outSize, sizeof(float));
+
+	Vec3f absVelocity, relVelocity;
+	float radVelocity;
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_data(format, RGL_FIELD_ABSOLUTE_VELOCITY_VEC3_F32, &absVelocity));
+	EXPECT_EQ(absVelocity.x(), cubeVelocity.x());
+	EXPECT_EQ(absVelocity.y(), cubeVelocity.y());
+	EXPECT_EQ(absVelocity.z(), cubeVelocity.z());
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_data(format, RGL_FIELD_RELATIVE_VELOCITY_VEC3_F32, &relVelocity));
+	EXPECT_EQ(relVelocity.x(), cubeVelocity.x());
+	EXPECT_EQ(relVelocity.y(), cubeVelocity.y());
+	EXPECT_EQ(relVelocity.z(), cubeVelocity.z());
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_data(format, RGL_FIELD_RADIAL_SPEED_F32, &radVelocity));
+	//    EXPECT_EQ(radVelocity, cubeVelocity);
+}
+
+constexpr float DIST = 5.0f;
+
+TEST_F(EntityVelocityUnitTests, StationaryLidarTranslatingEntity_Working_Version)
+{
+	rgl_node_t rays = nullptr, raytrace = nullptr, compact = nullptr, format = nullptr;
+	std::vector<rgl_mat3x4f> raysTf = {Mat3x4f::identity().toRGL()};
+	std::vector<rgl_field_t> velocityFields = {RGL_FIELD_ABSOLUTE_VELOCITY_VEC3_F32, RGL_FIELD_RELATIVE_VELOCITY_VEC3_F32,
+	                                           RGL_FIELD_RADIAL_SPEED_F32};
+
+	TestScene::DynamicCube translatingCube([](TestScene::DynamicCube& cube, float t) {
+		auto pose = Mat3x4f::translation(Vec3f{0, 0, DIST * t}).toRGL();
+		EXPECT_RGL_SUCCESS(rgl_entity_set_pose(cube.entity, &pose));
+	});
+
+
+	ASSERT_RGL_SUCCESS(rgl_node_rays_from_mat3x4f(&rays, raysTf.data(), raysTf.size()));
+	ASSERT_RGL_SUCCESS(rgl_node_raytrace(&raytrace, nullptr));
+	ASSERT_RGL_SUCCESS(rgl_node_points_compact(&compact));
+	ASSERT_RGL_SUCCESS(rgl_node_points_format(&format, velocityFields.data(), velocityFields.size()));
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(rays, raytrace));
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(raytrace, compact));
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(compact, format));
+
+	uint64_t frameId = 0;
+	uint64_t nsPerFrame = 10 * 1000 * 1000; // 10 ms per frame
+	for (int i = 0; i < 3; ++i) {
+		auto currentTime = frameId * nsPerFrame;
+		auto currentTimeSeconds = static_cast<double>(currentTime) / 1E9;
+		ASSERT_RGL_SUCCESS(rgl_scene_set_time(nullptr, currentTime));
+
+		translatingCube.update(currentTimeSeconds);
+
+		ASSERT_RGL_SUCCESS(rgl_graph_run(raytrace));
+		frameId += 1;
+	}
+
+	int32_t outCount, outSize;
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(format, RGL_FIELD_ABSOLUTE_VELOCITY_VEC3_F32, &outCount, &outSize));
+	EXPECT_EQ(outCount, raysTf.size());
+	EXPECT_EQ(outSize, sizeof(Vec3f));
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(format, RGL_FIELD_RELATIVE_VELOCITY_VEC3_F32, &outCount, &outSize));
+	EXPECT_EQ(outCount, raysTf.size());
+	EXPECT_EQ(outSize, sizeof(Vec3f));
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_size(format, RGL_FIELD_RADIAL_SPEED_F32, &outCount, &outSize));
+	EXPECT_EQ(outCount, raysTf.size());
+	EXPECT_EQ(outSize, sizeof(float));
+
+	Vec3f absVelocity, relVelocity;
+	float radVelocity;
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_data(format, RGL_FIELD_ABSOLUTE_VELOCITY_VEC3_F32, &absVelocity));
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_data(format, RGL_FIELD_RELATIVE_VELOCITY_VEC3_F32, &relVelocity));
+	EXPECT_RGL_SUCCESS(rgl_graph_get_result_data(format, RGL_FIELD_RADIAL_SPEED_F32, &radVelocity));
+	//    EXPECT_EQ(radVelocity, cubeVelocity);
+}
+
+
+/*
+ * Unit tests:
+ *
+ * RELATIVE:
+ * - stationary lidar, stationary entity: relative velocity should be zero; rel_vel = abs_vel
+ * - stationary lidar, moving entity: relative velocity should be equal to entity's velocity
+ * - moving lidar, stationary object: relative velocity should be equal to lidar's velocity, but in opposite direction
+ * - moving lidar, moving entity, same direction: relative velocity should be difference between lidar's and entity's velocity
+ * - moving lidar, moving entity, opposite direction: relative velocity should be sum of lidar's and entity's velocity
+ * - rotating lidar
+
+ * RADIAL:
+ * - stationary lidar, stationary entity: radial speed should be zero
+ * - stationary lidar, entity moving towards lidar: radial speed should be positive
+ * - stationary lidar, entity moving away from lidar: radial speed should be negative
+ *
+ * - rotating lidar, rotating entity (Earth, Moon)
+ * - stationary lidar, entity moving perpendicular to lidar's line of sight: radial speed should be zero (izolated test)
+ * - stationary lidar, entity moving at an angle to lidar's line of sight: radial speed should be not zero (cosine) - e.g. 45 deg
+ */
