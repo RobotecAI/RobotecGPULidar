e1b09f6caa47c89044baf83942057a71c0be13bf
nebraszka <majusxp@gmail.com>
2024-02-16 11:49:42 +0100

    Final implementation of tests

--- test/src/graph/nodes/Ros2PublishRadarScanNodeTest.cpp
+++ test/src/graph/nodes/Ros2PublishRadarScanNodeTest.cpp
@@ -1,5 +1,4 @@
 #include <helpers/commonHelpers.hpp>
-#include <helpers/sceneHelpers.hpp>
 #include <helpers/graphHelpers.hpp>
 #include <helpers/testPointCloud.hpp>
 
@@ -68,4 +67,25 @@ TEST_F(Ros2PublishRadarScanNodeTest, should_receive_sent_data)
 		         std::chrono::steady_clock::now() - start < std::chrono::seconds(WAIT_TIME_SECS));
 		ASSERT_EQ(messageCount, MESSAGE_REPEATS);
 	}
+}
+
+TEST_F(Ros2PublishRadarScanNodeTest, should_throw_invalid_pipeline_when_ros2_shutdown)
+{
+	std::vector<rgl_field_t> fields{XYZ_VEC3_F32, DISTANCE_F32, AZIMUTH_F32, ELEVATION_F32, RADIAL_SPEED_F32};
+	TestPointCloud pointCloud(fields, 10);
+	rgl_node_t points = pointCloud.createUsePointsNode();
+
+	rgl_node_t ros2radarscan = nullptr;
+	rgl_qos_policy_reliability_t qos_r = QOS_POLICY_RELIABILITY_BEST_EFFORT;
+	rgl_qos_policy_durability_t qos_d = QOS_POLICY_DURABILITY_VOLATILE;
+	rgl_qos_policy_history_t qos_h = QOS_POLICY_HISTORY_KEEP_LAST;
+
+	EXPECT_RGL_SUCCESS(rgl_node_publish_ros2_radarscan(&ros2radarscan, "radarscan", "rglFrame", qos_r, qos_d, qos_h, 10));
+
+	EXPECT_RGL_SUCCESS(rgl_graph_node_add_child(points, ros2radarscan));
+
+	EXPECT_RGL_SUCCESS(rgl_graph_run(points));
+
+	rclcpp::shutdown();
+	EXPECT_RGL_INVALID_PIPELINE(rgl_graph_run(points), "Unable to execute Ros2Node because ROS2 has been shut down.");
 }
