741ebc1f33da051aa45373f697ef2e065e4a2ebb
Mateusz Szczygielski <mateusz.szczygielski@robotec.ai>
2024-03-06 18:09:30 +0100

    Add more test cases

--- test/src/graph/nodes/RadarPostprocessPointsNodeTest.cpp
+++ test/src/graph/nodes/RadarPostprocessPointsNodeTest.cpp
@@ -17,14 +17,14 @@ TEST_F(RadarPostprocessPointsNodeTest, clustering_test)
 	std::array<rgl_radar_scope_t, 2> radarScopes = {
 	    rgl_radar_scope_t{ .begin_distance = 0.0f,
 	                      .end_distance = 10.0f,
-	                      .distance_separation_threshold = 1.0f,
-	                      .radial_speed_separation_threshold = 1.0f,
-	                      .azimuth_separation_threshold = 1.0f},
-	    rgl_radar_scope_t{.begin_distance = 10.0f,
-	                      .end_distance = 20.0f,
 	                      .distance_separation_threshold = 2.0f,
 	                      .radial_speed_separation_threshold = 2.0f,
 	                      .azimuth_separation_threshold = 2.0f},
+	    rgl_radar_scope_t{.begin_distance = 10.0f,
+	                      .end_distance = 20.0f,
+	                      .distance_separation_threshold = 1.0f,
+	                      .radial_speed_separation_threshold = 1.0f,
+	                      .azimuth_separation_threshold = 1.0f},
 	};
 	ASSERT_RGL_SUCCESS(rgl_node_points_radar_postprocess(&radarNode, radarScopes.data(), radarScopes.size(), 1.0f, 1.0f, 1.0f));
 
@@ -45,79 +45,82 @@ TEST_F(RadarPostprocessPointsNodeTest, clustering_test)
 	// TODO(msz-rai): add randomization
 	{
 		TestPointCloud inPointCloud(pointFields, 2);
-		inPointCloud.setFieldValues<DISTANCE_F32>({10, 10});
-		inPointCloud.setFieldValues<AZIMUTH_F32>({10, 10});
-		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10, 10});
-		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 1) << "Should merge the same points";
-	}
-	{
-		TestPointCloud inPointCloud(pointFields, 2);
-		inPointCloud.setFieldValues<DISTANCE_F32>({100, 200});
-		inPointCloud.setFieldValues<AZIMUTH_F32>({10, 10});
-		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10, 10});
-		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 0) << "Should reject all points (out of radar scope case)";
+		inPointCloud.setFieldValues<DISTANCE_F32>({100.0f, 200.0f});
+		inPointCloud.setFieldValues<AZIMUTH_F32>({10.0f, 10.0f});
+		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10.0f, 10.0f});
+		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 0) << "Should reject all points (out of radar scope)";
 	}
 	{
 		TestPointCloud inPointCloud(pointFields, 3);
-		inPointCloud.setFieldValues<DISTANCE_F32>({5.0f, 5.5f, 6.0f});
-		inPointCloud.setFieldValues<AZIMUTH_F32>({10, 10.1f, 10.2f});
-		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10, 10.1f, 10.2f});
+		inPointCloud.setFieldValues<DISTANCE_F32>({5.0f, 6.0f, 7.0f});
+		inPointCloud.setFieldValues<AZIMUTH_F32>({10.0f, 10.1f, 10.2f});
+		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10.0f, 10.1f, 10.2f});
 		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 1)
-		    << "Should merge the same points that meet all separation thresholds";
+		    << "Creating clusters phase: Should merge points into one cluster (they all meet separation thresholds)";
 	}
 	{
 		TestPointCloud inPointCloud(pointFields, 2);
-		inPointCloud.setFieldValues<DISTANCE_F32>({5, 10});
-		inPointCloud.setFieldValues<AZIMUTH_F32>({10, 10});
-		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10, 10});
-		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 2) << "Should not merge because of distance";
+		inPointCloud.setFieldValues<DISTANCE_F32>({5.0f, 10.0f});
+		inPointCloud.setFieldValues<AZIMUTH_F32>({10.0f, 10.0f});
+		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10.0f, 10.0f});
+		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 2)
+		    << "Creating clusters phase: Should not merge because of distance";
 	}
 	{
 		TestPointCloud inPointCloud(pointFields, 2);
-		inPointCloud.setFieldValues<DISTANCE_F32>({5, 5});
-		inPointCloud.setFieldValues<AZIMUTH_F32>({10, 20});
-		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10, 10});
-		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 2) << "Should not merge because of azimuth";
+		inPointCloud.setFieldValues<DISTANCE_F32>({5.0f, 5.0f});
+		inPointCloud.setFieldValues<AZIMUTH_F32>({10.0f, 20.0f});
+		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10.0f, 10.0f});
+		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 2)
+		    << "Creating clusters phase: Should not merge because of azimuth";
 	}
 	{
 		TestPointCloud inPointCloud(pointFields, 2);
-		inPointCloud.setFieldValues<DISTANCE_F32>({5, 5});
-		inPointCloud.setFieldValues<AZIMUTH_F32>({10, 10});
-		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10, 20});
-		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 2) << "Should not merge because of radial speed";
+		inPointCloud.setFieldValues<DISTANCE_F32>({5.0f, 5.0f});
+		inPointCloud.setFieldValues<AZIMUTH_F32>({10.0f, 10.0f});
+		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10.0f, 20.0f});
+		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 2)
+		    << "Creating clusters phase: Should not merge because of radial speed";
 	}
 	{
-		TestPointCloud inPointCloud(pointFields, 6);
-		inPointCloud.setFieldValues<DISTANCE_F32>({5.0f, 7.5f, 6.0f, 6.5f, 7.0f, 5.5f});
-		inPointCloud.setFieldValues<AZIMUTH_F32>({10, 10, 10, 10, 10, 10});
-		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10, 10, 10, 10, 10, 10});
+		TestPointCloud inPointCloud(pointFields, 3);
+		inPointCloud.setFieldValues<DISTANCE_F32>({0.0f, 2.5f, 1.0f});   // point[1] is separate cluster after first iteration
+		inPointCloud.setFieldValues<AZIMUTH_F32>({11.0f, 12.5f, 12.0f}); // azimuth[1] is within separation threshold
+		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({11.0f, 12.5f, 12.0f}); // speed[1] is within separation threshold
 		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 1)
-		    << "Should merge all points, but order is not straightforward (clusters merging is triggered)";
+		    << "Merging clusters phase: Should merge all clusters (attributes within separation threshold)";
 	}
 	{
-		TestPointCloud inPointCloud(pointFields, 6);
-		inPointCloud.setFieldValues<DISTANCE_F32>({5.0f, 7.5f, 6.0f, 6.5f, 7.0f, 5.5f});
-		inPointCloud.setFieldValues<AZIMUTH_F32>({10, 20, 10, 10, 10, 10});
-		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10, 10, 10, 10, 10, 10});
-		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 2)
-		    << "Should not merge all points because of azimuth. Points order is not straightforward (clusters merging is "
-		       "triggered)";
+		TestPointCloud inPointCloud(pointFields, 3);
+		inPointCloud.setFieldValues<DISTANCE_F32>({0.0f, 2.5f, 1.0f});   // point[1] is separate cluster after first iteration
+		inPointCloud.setFieldValues<AZIMUTH_F32>({11.0f, 11.5f, 12.0f}); // azimuth[1] is inside cluster azimuth range
+		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({11.0f, 11.5f, 12.0f}); // speed[1] is inside cluster speed range
+		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 1)
+		    << "Merging clusters phase: Should merge all clusters (attributes inside cluster range)";
 	}
 	{
-		TestPointCloud inPointCloud(pointFields, 6);
-		inPointCloud.setFieldValues<DISTANCE_F32>({5.0f, 7.5f, 6.0f, 6.5f, 7.0f, 5.5f});
-		inPointCloud.setFieldValues<AZIMUTH_F32>({10, 10, 10, 10, 10, 10});
-		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10, 20, 10, 10, 10, 10});
+		TestPointCloud inPointCloud(pointFields, 3);
+		inPointCloud.setFieldValues<DISTANCE_F32>({0.0f, 2.5f, 1.0f});  // point[1] is separate cluster after first iteration
+		inPointCloud.setFieldValues<AZIMUTH_F32>({11.0f, 0.0f, 12.0f}); // azimuth[1] out of separation threshold
+		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({11.0f, 11.5f, 12.0f}); // speed[1] is within separation threshold
 		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 2)
-		    << "Should not merge  all points because of radial speed. Points order is not straightforward (clusters merging is "
-		       "triggered)";
+		    << "Merging clusters phase: Should not merge one cluster because of azimuth";
+	}
+	{
+		TestPointCloud inPointCloud(pointFields, 3);
+		inPointCloud.setFieldValues<DISTANCE_F32>({0.0f, 2.5f, 1.0f});   // point[1] is separate cluster after first iteration
+		inPointCloud.setFieldValues<AZIMUTH_F32>({11.0f, 11.5f, 12.0f}); // azimuth[1] is within separation threshold
+		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({11.0f, 0.0f, 12.0f}); // speed[1] out of separation threshold
+		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 2)
+		    << "Merging clusters phase: Should not merge one cluster because of radial speed";
 	}
 	{
 		TestPointCloud inPointCloud(pointFields, 4);
-		inPointCloud.setFieldValues<DISTANCE_F32>({13.0f, 11.5f, 10.0f, 8.5f});
-		inPointCloud.setFieldValues<AZIMUTH_F32>({10, 10, 10, 10});
-		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10, 10, 10, 10});
+		inPointCloud.setFieldValues<DISTANCE_F32>({6.5f, 8.0f, 9.5f, 11.0f}); // last point is next scope with threshold=1
+		inPointCloud.setFieldValues<AZIMUTH_F32>({10.0f, 10.0f, 10.0f, 10.0f});
+		inPointCloud.setFieldValues<RADIAL_SPEED_F32>({10.0f, 10.0f, 10.0f, 10.0f});
 		ASSERT_EQ(runAndReturnNumberOfClusters(inPointCloud), 2)
-		    << "Should not merge all points because different scope is reached with smaller separation threshold";
+		    << "Creating clusters phase: Should not merge all points because different scope is reached with smaller "
+		       "separation threshold";
 	}
 }
