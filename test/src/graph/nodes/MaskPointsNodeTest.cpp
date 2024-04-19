4629a12bb6b53f70e555f72e89e4bcf0886910f5
PiotrMrozik <piter012@vp.pl>
2024-04-19 13:55:15 +0200

    fix test error

--- test/src/graph/nodes/MaskPointsNodeTest.cpp
+++ test/src/graph/nodes/MaskPointsNodeTest.cpp
@@ -6,23 +6,21 @@ class MaskPointsNodeTest : public RGLTest
 {
 
 protected:
-	int pointsCount = 10000;
+	int pointsCount;
 	std::vector<int32_t> points_mask;
 	std::vector<rgl_field_t> fields = {XYZ_VEC3_F32, IS_HIT_I32, INTENSITY_F32, IS_GROUND_I32};
 
-	void initializeMask(int pointCount)
+	void initializeMask(int pointCount, int maskCount)
 	{
 		points_mask.resize(pointsCount);
-		for (int i = 0; i < pointCount; i++) {
-			points_mask[i] = i % 2 == 0;
+		std::fill(points_mask.begin(), points_mask.end(), 1);
+		for (int i = 0; i < maskCount; i++) {
+			points_mask[i] = 0;
 		}
 	}
 };
 
-TEST_F(MaskPointsNodeTest, invalid_argument_node)
-{
-	EXPECT_RGL_INVALID_ARGUMENT(rgl_node_mask_points(nullptr, nullptr, 0));
-}
+TEST_F(MaskPointsNodeTest, invalid_argument_node) { EXPECT_RGL_INVALID_ARGUMENT(rgl_node_mask_points(nullptr, nullptr, 0)); }
 
 TEST_F(MaskPointsNodeTest, invalid_argument_mask)
 {
@@ -39,7 +37,7 @@ TEST_F(MaskPointsNodeTest, invalid_argument_count)
 TEST_F(MaskPointsNodeTest, valid_arguments)
 {
 	rgl_node_t maskPointsNode = nullptr;
-	initializeMask(pointsCount);
+	initializeMask(pointsCount, 100);
 	EXPECT_RGL_SUCCESS(rgl_node_mask_points(&maskPointsNode, points_mask.data(), 1));
 	ASSERT_THAT(maskPointsNode, testing::NotNull());
 }
@@ -49,8 +47,9 @@ TEST_F(MaskPointsNodeTest, use_case)
 	TestPointCloud pointCloud = TestPointCloud(fields, pointsCount);
 	pointCloud.removeNonHitPoints();
 	pointsCount = pointCloud.getPointCount();
+	int maskCount = pointsCount / 2;
 
-	initializeMask(pointsCount);
+	initializeMask(pointsCount, maskCount);
 
 	rgl_node_t usePointsNode = pointCloud.createUsePointsNode();
 	rgl_node_t maskPointsNode = nullptr;
@@ -67,5 +66,5 @@ TEST_F(MaskPointsNodeTest, use_case)
 
 	TestPointCloud outputPointCloud = TestPointCloud::createFromNode(compactNode, fields);
 
-	ASSERT_EQ(outputPointCloud.getPointCount(), pointsCount / 2);
+	ASSERT_EQ(outputPointCloud.getPointCount(), pointsCount - maskCount);
 }
