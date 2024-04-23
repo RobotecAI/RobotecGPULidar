cf7ebec8b499bae74719b545f49feece9c429bfc
Paweł Liberadzki <pawel.liberadzki@robotec.ai>
2024-04-23 14:49:57 +0200

    Add fixed objects count test. Make some cleanup in object tracking tests.
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>

--- test/src/graph/nodes/RadarTrackObjectsNodeTest.cpp
+++ test/src/graph/nodes/RadarTrackObjectsNodeTest.cpp
@@ -5,6 +5,7 @@
 #include <random>
 #include <ranges>
 
+
 class RadarTrackObjectsNodeTest : public RGLTest
 {};
 
@@ -31,25 +32,15 @@ Vec3f getRandomVector()
 	return Vec3f{std::sin(theta) * std::cos(phi), std::sin(theta) * std::sin(phi), std::cos(theta)};
 }
 
-void GenerateDetectionClusters(TestPointCloud& pointCloud, size_t clusterCount, size_t clusterPointsCount)
+void generateDetectionClusters(TestPointCloud& pointCloud, const std::vector<Vec3f>& clusterCenters, size_t clusterPointsCount)
 {
-	constexpr float centerScale = 10.0f;
-	constexpr float centerXOffset = 20.0f;
-
-	std::vector<Vec3f> clusterCenters;
-	std::generate_n(std::back_inserter(clusterCenters), clusterCount, getRandomVector);
-
 	std::vector<Vec3f> xyz;
 	std::vector<float> distance;
 	std::vector<float> azimuth;
 	std::vector<float> elevation;
 	std::vector<float> radialSpeed;
 
-	for (const auto& clusterCenter : std::views::transform(clusterCenters, [=](auto center) {
-		     center *= centerScale;
-		     center[0] = center[0] + centerXOffset;
-		     return center;
-	     })) {
+	for (const auto clusterCenter : clusterCenters) {
 		const auto clusterXYZ = generateFieldValues(clusterPointsCount, genNormal);
 		for (const auto& detectionXYZ : clusterXYZ) {
 			const auto worldXYZ = detectionXYZ + clusterCenter;
@@ -70,11 +61,60 @@ void GenerateDetectionClusters(TestPointCloud& pointCloud, size_t clusterCount, 
 	pointCloud.setFieldValues<AZIMUTH_F32>(azimuth);
 	pointCloud.setFieldValues<ELEVATION_F32>(elevation);
 	pointCloud.setFieldValues<RADIAL_SPEED_F32>(radialSpeed);
-
-	std::printf("");
 }
 
-TEST_F(RadarTrackObjectsNodeTest, creating_objects_test)
+void generateFixedDetectionClusters(TestPointCloud& pointCloud, size_t clusterCount, size_t clusterPointsCount)
+{
+	constexpr float centerScale = 10.0f;
+	const Vec3f centerXOffset = Vec3f{20.0f, 0.0f, 0.0f};
+
+	std::vector<Vec3f> clusterCenters;
+	for (int i = 0; i < clusterCount; ++i) {
+		const auto angle = i * 2 * M_PI / static_cast<double>(clusterCount);
+		clusterCenters.emplace_back(Vec3f{std::cos(angle), std::sin(angle), 0.0f} * centerScale);
+	}
+
+	generateDetectionClusters(pointCloud, clusterCenters, clusterPointsCount);
+}
+
+void generateRandomDetectionClusters(TestPointCloud& pointCloud, size_t clusterCount, size_t clusterPointsCount)
+{
+	constexpr float centerScale = 10.0f;
+	const Vec3f centerOffset = Vec3f{20.0f, 0.0f, 0.0f};
+
+	std::vector<Vec3f> clusterCenters;
+	std::generate_n(std::back_inserter(clusterCenters), clusterCount,
+	                [&]() { return getRandomVector() * centerScale + centerOffset; });
+
+	generateDetectionClusters(pointCloud, clusterCenters, clusterPointsCount);
+}
+
+TEST_F(RadarTrackObjectsNodeTest, objects_number_test)
+{
+	std::vector<rgl_field_t> fields{XYZ_VEC3_F32};
+
+	// Setup objects tracking node
+	rgl_node_t trackObjectsNode = nullptr;
+	ASSERT_RGL_SUCCESS(rgl_node_points_radar_track_objects(&trackObjectsNode));
+
+	constexpr size_t objectsCount = 5;
+	constexpr size_t detectionsCountPerObject = 10;
+	std::vector<rgl_field_t> pointFields =
+	    Node::validatePtr<RadarTrackObjectsNode>(trackObjectsNode)->getRequiredFieldList();
+	TestPointCloud inPointCloud(pointFields, objectsCount * detectionsCountPerObject);
+
+	generateFixedDetectionClusters(inPointCloud, objectsCount, detectionsCountPerObject);
+
+	const auto usePointsNode = inPointCloud.createUsePointsNode();
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(usePointsNode, trackObjectsNode));
+	ASSERT_RGL_SUCCESS(rgl_graph_run(trackObjectsNode));
+
+	int32_t detectedObjectsCount = 0, objectsSize = 0;
+	rgl_graph_get_result_size(trackObjectsNode, fields.at(0), &detectedObjectsCount, &objectsSize);
+	ASSERT_TRUE(detectedObjectsCount == objectsCount);
+}
+
+TEST_F(RadarTrackObjectsNodeTest, creating_random_objects_test)
 {
 	GTEST_SKIP_("Debug test on development stage.");
 
@@ -83,8 +123,8 @@ TEST_F(RadarTrackObjectsNodeTest, creating_objects_test)
 
 	while (true) {
 		// Setup objects tracking node
-		rgl_node_t trackObjectsNode = nullptr, ros2DetectionsNode = nullptr, ros2ObjectsNode = nullptr, detectionsFormat = nullptr,
-		           objectsFormat = nullptr;
+		rgl_node_t trackObjectsNode = nullptr, ros2DetectionsNode = nullptr, ros2ObjectsNode = nullptr,
+		           detectionsFormat = nullptr, objectsFormat = nullptr;
 		ASSERT_RGL_SUCCESS(rgl_node_points_radar_track_objects(&trackObjectsNode));
 		ASSERT_RGL_SUCCESS(rgl_node_points_ros2_publish(&ros2DetectionsNode, "radar_detections", "world"));
 		ASSERT_RGL_SUCCESS(rgl_node_points_ros2_publish(&ros2ObjectsNode, "radar_objects", "world"));
@@ -93,21 +133,22 @@ TEST_F(RadarTrackObjectsNodeTest, creating_objects_test)
 
 		const size_t objectsCount = getRandomValue<int, 5, 10>();
 		const size_t detectionsCountPerObject = getRandomValue<int, 10, 20>();
-		std::vector<rgl_field_t> pointFields = Node::validatePtr<RadarTrackObjectsNode>(trackObjectsNode)->getRequiredFieldList();
+		std::vector<rgl_field_t> pointFields =
+		    Node::validatePtr<RadarTrackObjectsNode>(trackObjectsNode)->getRequiredFieldList();
 		TestPointCloud inPointCloud(pointFields, objectsCount * detectionsCountPerObject);
 
-		GenerateDetectionClusters(inPointCloud, objectsCount, detectionsCountPerObject);
+		generateRandomDetectionClusters(inPointCloud, objectsCount, detectionsCountPerObject);
 
 		const auto usePointsNode = inPointCloud.createUsePointsNode();
-		EXPECT_RGL_SUCCESS(rgl_graph_node_add_child(usePointsNode, trackObjectsNode));
+		ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(usePointsNode, trackObjectsNode));
 
-		EXPECT_RGL_SUCCESS(rgl_graph_node_add_child(usePointsNode, detectionsFormat));
-		EXPECT_RGL_SUCCESS(rgl_graph_node_add_child(detectionsFormat, ros2DetectionsNode));
+		ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(usePointsNode, detectionsFormat));
+		ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(detectionsFormat, ros2DetectionsNode));
 
-		EXPECT_RGL_SUCCESS(rgl_graph_node_add_child(trackObjectsNode, objectsFormat));
-		EXPECT_RGL_SUCCESS(rgl_graph_node_add_child(objectsFormat, ros2ObjectsNode));
+		ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(trackObjectsNode, objectsFormat));
+		ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(objectsFormat, ros2ObjectsNode));
 
-		EXPECT_RGL_SUCCESS(rgl_graph_run(trackObjectsNode));
+		ASSERT_RGL_SUCCESS(rgl_graph_run(trackObjectsNode));
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
 		int32_t detectedObjectsCount = 0, objectsSize = 0;
