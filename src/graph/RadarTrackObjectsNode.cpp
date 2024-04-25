ff5d4e79aacf56b27d87e3b2f4466dd8ae13781e
PawelLiberadzki <44069826+PawelLiberadzki@users.noreply.github.com>
2024-04-25 17:39:26 +0200

    Add node for radar object tracking (#280)
    * Add radar object tracking node and test placeholders.
    
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>
    
    * Add test for basic node run.
    
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>
    
    * Add first implementation for detections clusterization.
    
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>
    
    * Add working detections clusterization.
    
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>
    
    * Add complete debug test. Make minor cleanup in node implementation.
    
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>
    
    * Add milliseconds cast to helper Time structure.
    
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>
    
    * Add disabling Vector initializer list constructor, when there is only one dimension.
    
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>
    
    * Add RunningStats helper structer for running mean and standard deviation.
    
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>
    
    * Add basic object state handling to RadarTrackObjectsNode.
    
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>
    
    * Add fixed objects count test. Make some cleanup in object tracking tests.
    
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>
    
    * Fix ROS 2 for radar track objects test.
    
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>
    
    * Update local constants to be node parameters. Add API call documentation for object tracking.
    
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>
    
    * Update object tracking node documentation with parameter units.
    
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>
    
    * Make changes according to review for #280.
    
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>
    
    ---------
    
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>

---
+++ src/graph/RadarTrackObjectsNode.cpp
@@ -0,0 +1,128 @@
+// Copyright 2024 Robotec.AI
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <graph/NodesCore.hpp>
+#include <scene/Scene.hpp>
+
+RadarTrackObjectsNode::RadarTrackObjectsNode() { fieldData.emplace(XYZ_VEC3_F32, createArray<HostPinnedArray>(XYZ_VEC3_F32)); }
+
+void RadarTrackObjectsNode::setParameters(float distanceThreshold, float azimuthThreshold, float elevationThreshold,
+                                          float radialSpeedThreshold)
+{
+	this->distanceThreshold = distanceThreshold;
+	this->azimuthThreshold = azimuthThreshold;
+	this->elevationThreshold = elevationThreshold;
+	this->radialSpeedThreshold = radialSpeedThreshold;
+}
+
+void RadarTrackObjectsNode::validateImpl() { IPointsNodeSingleInput::validateImpl(); }
+
+void RadarTrackObjectsNode::enqueueExecImpl()
+{
+	xyzHostPtr->copyFrom(input->getFieldData(XYZ_VEC3_F32));
+	distanceHostPtr->copyFrom(input->getFieldData(DISTANCE_F32));
+	azimuthHostPtr->copyFrom(input->getFieldData(AZIMUTH_F32));
+	elevationHostPtr->copyFrom(input->getFieldData(ELEVATION_F32));
+	radialSpeedHostPtr->copyFrom(input->getFieldData(RADIAL_SPEED_F32));
+
+	const int detectionsCount = input->getPointCount();
+
+	objectStates.clear(); // TODO(Pawel): Remove this when we start working on tracking.
+
+	// Top level in this list is for objects. Bottom level is for detections that belong to specific objects. Below is an initialization of a helper
+	// structure for region growing, which starts with a while-loop.
+	std::list<std::list<int>> objectIndices;
+	for (auto i = 0; i < detectionsCount; ++i) {
+		objectIndices.emplace_back(1, i);
+	}
+
+	auto leftIndex = 0;
+	while (leftIndex < objectIndices.size() - 1) {
+		auto ownIt = objectIndices.begin();
+		std::advance(ownIt, leftIndex);
+		auto checkedIt = std::next(ownIt);
+		bool reorganizationTookPlace = false;
+
+		while (checkedIt != objectIndices.end()) {
+			auto& checkedIndices = *checkedIt;
+
+			// TODO(Pawel): In theory, I do not have to check the same indices multiple times when growing this subset in next iterations.
+			for (auto ownDetectionId : *ownIt) {
+				const auto isPartOfSameObject = [&, distance = distanceHostPtr->at(ownDetectionId),
+				                                 azimuth = azimuthHostPtr->at(ownDetectionId),
+				                                 elevation = elevationHostPtr->at(ownDetectionId),
+				                                 radialSpeed = radialSpeedHostPtr->at(ownDetectionId)](int checkedDetectionId) {
+					return std::abs(distanceHostPtr->at(checkedDetectionId) - distance) <= distanceThreshold &&
+					       std::abs(azimuthHostPtr->at(checkedDetectionId) - azimuth) <= azimuthThreshold &&
+					       std::abs(elevationHostPtr->at(checkedDetectionId) - elevation) <= elevationThreshold &&
+					       std::abs(radialSpeedHostPtr->at(checkedDetectionId) - radialSpeed) <= radialSpeedThreshold;
+				};
+
+				if (std::any_of(checkedIndices.cbegin(), checkedIndices.cend(), isPartOfSameObject)) {
+					ownIt->splice(ownIt->cend(), checkedIndices);
+					break;
+				}
+			}
+			if (checkedIndices.empty()) {
+				checkedIt = objectIndices.erase(checkedIt);
+				reorganizationTookPlace = true;
+			} else {
+				++checkedIt;
+			}
+		}
+
+		if (!reorganizationTookPlace) {
+			// We are done with growing this object, no more matching detections are available.
+			++leftIndex;
+		}
+	}
+
+	fieldData[XYZ_VEC3_F32]->resize(objectIndices.size(), true, false);
+	auto* xyzPtr = static_cast<Vec3f*>(fieldData[XYZ_VEC3_F32]->getRawWritePtr());
+	uint32_t objectIndex = 0;
+
+	for (const auto& separateObjectIndices : objectIndices) {
+		auto objectCenter = Vec3f(0.0f, 0.0f, 0.0f);
+		for (const auto index : separateObjectIndices) {
+			objectCenter += xyzHostPtr->at(index);
+		}
+
+		auto& objectState = objectStates.emplace_back();
+		objectState.id = objectIndex++;
+		objectState.framesCount = 1; // This will be updated with object tracking introduction.
+		objectState.creationTime = static_cast<uint32_t>(Scene::instance().getTime().value_or(Time::zero()).asMilliseconds());
+		objectState.objectStatus = ObjectStatus::New;
+		objectState.movementStatus =
+		    MovementStatus::Invalid; // Newly created object does not have velocity data (no 'previous frame' exists).
+		objectState.position.addSample(objectCenter / static_cast<float>(separateObjectIndices.size()));
+
+		// TODO(Pawel): This will require modyfing radar postprocess node - I will require some bounding box here.
+		objectState.orientation.addSample(0.0f);
+		objectState.absVelocity.addSample(Vec2f(0.0f, 0.0f));
+		objectState.relVelocity.addSample(Vec2f(0.0f, 0.0f));
+		objectState.absAccel.addSample(Vec2f(0.0f, 0.0f));
+		objectState.relAccel.addSample(Vec2f(0.0f, 0.0f));
+		objectState.orientationRate.addSample(0.0f);
+		objectState.length.addSample(0.0f);
+		objectState.width.addSample(0.0f);
+
+		// TODO(Pawel): Be careful with indexing here, when working on tracking - id will not match the index for not new objects.
+		xyzPtr[objectState.id] = 1 / static_cast<float>(separateObjectIndices.size()) * objectCenter;
+	}
+}
+
+std::vector<rgl_field_t> RadarTrackObjectsNode::getRequiredFieldList() const
+{
+	return {XYZ_VEC3_F32, DISTANCE_F32, AZIMUTH_F32, ELEVATION_F32, RADIAL_SPEED_F32};
+}
---
+++ test/src/graph/nodes/RadarTrackObjectsNodeTest.cpp
@@ -0,0 +1,173 @@
+#include <api/apiCommon.hpp>
+#include <helpers/testPointCloud.hpp>
+
+#include <random>
+
+
+class RadarTrackObjectsNodeTest : public RGLTest
+{};
+
+template<typename Type, Type MinV, Type MaxV>
+Type getRandomValue()
+{
+	static_assert(std::is_arithmetic_v<Type>, "Template arguments are not numbers.");
+	static std::uniform_real_distribution<float> distribution(MinV, MaxV);
+
+	const auto seed = std::random_device{}();
+	std::mt19937 generator(seed);
+
+	return distribution(generator);
+}
+
+Vec3f getRandomVector()
+{
+	std::uniform_real_distribution<> distribution(0, 2 * M_PI);
+
+	const auto seed = std::random_device{}();
+	std::mt19937 generator(seed);
+
+	double theta = distribution(generator), phi = distribution(generator);
+	return Vec3f{std::sin(theta) * std::cos(phi), std::sin(theta) * std::sin(phi), std::cos(theta)};
+}
+
+void generateDetectionClusters(TestPointCloud& pointCloud, const std::vector<Vec3f>& clusterCenters, size_t clusterPointsCount)
+{
+	std::vector<Vec3f> xyz;
+	std::vector<float> distance;
+	std::vector<float> azimuth;
+	std::vector<float> elevation;
+	std::vector<float> radialSpeed;
+
+	for (const auto clusterCenter : clusterCenters) {
+		const auto clusterXYZ = generateFieldValues(clusterPointsCount, genNormal);
+		for (const auto& detectionXYZ : clusterXYZ) {
+			const auto worldXYZ = detectionXYZ + clusterCenter;
+
+			xyz.emplace_back(worldXYZ);
+			distance.emplace_back(worldXYZ.length());
+
+			const auto worldSph = worldXYZ.toSpherical();
+			azimuth.emplace_back(worldSph[1]);
+			elevation.emplace_back(worldSph[2]);
+
+			radialSpeed.emplace_back(getRandomValue<float, 4.8f, 5.2f>());
+		}
+	}
+
+	pointCloud.setFieldValues<XYZ_VEC3_F32>(xyz);
+	pointCloud.setFieldValues<DISTANCE_F32>(distance);
+	pointCloud.setFieldValues<AZIMUTH_F32>(azimuth);
+	pointCloud.setFieldValues<ELEVATION_F32>(elevation);
+	pointCloud.setFieldValues<RADIAL_SPEED_F32>(radialSpeed);
+}
+
+void generateFixedDetectionClusters(TestPointCloud& pointCloud, size_t clusterCount, size_t clusterPointsCount)
+{
+	constexpr float centerScale = 10.0f;
+	const Vec3f centerXOffset = Vec3f{20.0f, 0.0f, 0.0f};
+
+	std::vector<Vec3f> clusterCenters;
+	for (int i = 0; i < clusterCount; ++i) {
+		const auto angle = i * 2 * M_PI / static_cast<double>(clusterCount);
+		clusterCenters.emplace_back(Vec3f{std::cos(angle), std::sin(angle), 0.0f} * centerScale);
+	}
+
+	generateDetectionClusters(pointCloud, clusterCenters, clusterPointsCount);
+}
+
+void generateRandomDetectionClusters(TestPointCloud& pointCloud, size_t clusterCount, size_t clusterPointsCount)
+{
+	constexpr float centerScale = 10.0f;
+	const Vec3f centerOffset = Vec3f{20.0f, 0.0f, 0.0f};
+
+	std::vector<Vec3f> clusterCenters;
+	std::generate_n(std::back_inserter(clusterCenters), clusterCount,
+	                [&]() { return getRandomVector() * centerScale + centerOffset; });
+
+	generateDetectionClusters(pointCloud, clusterCenters, clusterPointsCount);
+}
+
+TEST_F(RadarTrackObjectsNodeTest, objects_number_test)
+{
+	std::vector<rgl_field_t> fields{XYZ_VEC3_F32};
+	constexpr float distanceThreshold = 2.0f;
+	constexpr float azimuthThreshold = 0.1f;
+	constexpr float elevationThreshold = 0.1f;
+	constexpr float radialSpeedThreshold = 0.5f;
+
+	// Setup objects tracking node
+	rgl_node_t trackObjectsNode = nullptr;
+	ASSERT_RGL_SUCCESS(rgl_node_points_radar_track_objects(&trackObjectsNode, distanceThreshold, azimuthThreshold,
+	                                                       elevationThreshold, radialSpeedThreshold));
+
+	constexpr size_t objectsCount = 5;
+	constexpr size_t detectionsCountPerObject = 10;
+	std::vector<rgl_field_t> pointFields = Node::validatePtr<RadarTrackObjectsNode>(trackObjectsNode)->getRequiredFieldList();
+	TestPointCloud inPointCloud(pointFields, objectsCount * detectionsCountPerObject);
+
+	generateFixedDetectionClusters(inPointCloud, objectsCount, detectionsCountPerObject);
+
+	const auto usePointsNode = inPointCloud.createUsePointsNode();
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(usePointsNode, trackObjectsNode));
+	ASSERT_RGL_SUCCESS(rgl_graph_run(trackObjectsNode));
+
+	int32_t detectedObjectsCount = 0, objectsSize = 0;
+	rgl_graph_get_result_size(trackObjectsNode, fields.at(0), &detectedObjectsCount, &objectsSize);
+	ASSERT_TRUE(detectedObjectsCount == objectsCount);
+}
+
+#if RGL_BUILD_ROS2_EXTENSION
+#include <rgl/api/extensions/ros2.h>
+TEST_F(RadarTrackObjectsNodeTest, creating_random_objects_test)
+{
+	GTEST_SKIP_("Debug test on development stage.");
+
+	std::vector<rgl_field_t> fields{XYZ_VEC3_F32};
+	constexpr float distanceThreshold = 2.0f;
+	constexpr float azimuthThreshold = 0.1f;
+	constexpr float elevationThreshold = 0.1f;
+	constexpr float radialSpeedThreshold = 0.5f;
+	size_t iterationCounter = 0;
+
+	while (true) {
+		// Setup objects tracking node
+		rgl_node_t trackObjectsNode = nullptr, ros2DetectionsNode = nullptr, ros2ObjectsNode = nullptr,
+		           detectionsFormat = nullptr, objectsFormat = nullptr;
+		ASSERT_RGL_SUCCESS(rgl_node_points_radar_track_objects(&trackObjectsNode, distanceThreshold, azimuthThreshold,
+		                                                       elevationThreshold, radialSpeedThreshold));
+		ASSERT_RGL_SUCCESS(rgl_node_points_ros2_publish(&ros2DetectionsNode, "radar_detections", "world"));
+		ASSERT_RGL_SUCCESS(rgl_node_points_ros2_publish(&ros2ObjectsNode, "radar_objects", "world"));
+		ASSERT_RGL_SUCCESS(rgl_node_points_format(&detectionsFormat, fields.data(), fields.size()));
+		ASSERT_RGL_SUCCESS(rgl_node_points_format(&objectsFormat, fields.data(), fields.size()));
+
+		const size_t objectsCount = getRandomValue<int, 5, 10>();
+		const size_t detectionsCountPerObject = getRandomValue<int, 10, 20>();
+		std::vector<rgl_field_t> pointFields =
+		    Node::validatePtr<RadarTrackObjectsNode>(trackObjectsNode)->getRequiredFieldList();
+		TestPointCloud inPointCloud(pointFields, objectsCount * detectionsCountPerObject);
+
+		generateRandomDetectionClusters(inPointCloud, objectsCount, detectionsCountPerObject);
+
+		const auto usePointsNode = inPointCloud.createUsePointsNode();
+		ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(usePointsNode, trackObjectsNode));
+
+		ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(usePointsNode, detectionsFormat));
+		ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(detectionsFormat, ros2DetectionsNode));
+
+		ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(trackObjectsNode, objectsFormat));
+		ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(objectsFormat, ros2ObjectsNode));
+
+		ASSERT_RGL_SUCCESS(rgl_graph_run(trackObjectsNode));
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+		int32_t detectedObjectsCount = 0, objectsSize = 0;
+		rgl_graph_get_result_size(trackObjectsNode, fields.at(0), &detectedObjectsCount, &objectsSize);
+
+		if (detectedObjectsCount != objectsCount) {
+			printf("[%lu] Detected / given objects: %d / %lu\n", iterationCounter++, detectedObjectsCount, objectsCount);
+		}
+
+		EXPECT_RGL_SUCCESS(rgl_cleanup());
+	}
+}
+#endif
