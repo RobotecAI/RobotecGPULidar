39d8f90c831c639eafee8fa830104dd0266a8d47
Paweł Liberadzki <pawel.liberadzki@robotec.ai>
2024-05-09 14:31:16 +0200

    Add covariance calculation.  Add RunningStats specialization for Vector types.
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>

--- src/math/RunningStats.hpp
+++ src/math/RunningStats.hpp
@@ -14,24 +14,29 @@
 
 #pragma once
 
-template<template <int, typename> class Base, class Checked>
-struct is_specialization_of : std::false_type { };
+template<template<int, typename> class Base, class Checked>
+struct is_specialization_of : std::false_type
+{};
 
-template<template <int, typename> class Base, int dim, typename T>
-struct is_specialization_of<Base, Base<dim, T>> : std::true_type { };
+template<template<int, typename> class Base, int dim, typename T>
+struct is_specialization_of<Base, Base<dim, T>> : std::true_type
+{};
 
-template <typename>
+template<typename>
 inline constexpr bool is_floating_point_vector_v = false;
 
-template <template <int, typename> class Base, int dim, typename T>
-inline constexpr bool is_floating_point_vector_v<Base<dim, T>> = std::is_floating_point_v<T> && is_specialization_of<Base, Vector<dim, T>>::value;
+template<template<int, typename> class Base, int dim, typename T>
+inline constexpr bool is_floating_point_vector_v<Base<dim, T>> = std::is_floating_point_v<T> &&
+                                                                 is_specialization_of<Base, Vector<dim, T>>::value;
 
 // Integral types here generally make no sense in terms of mean and std dev.
 // Floating point types and any Vector specializations using floating point types are acceptable.
-template<typename StatType, typename = std::enable_if_t<std::is_floating_point_v<StatType> || is_floating_point_vector_v<StatType>>>
+template<typename T, typename = std::enable_if_t<std::is_floating_point_v<T> || is_floating_point_vector_v<T>>>
 class RunningStats
 {
 public:
+	using StatType = T;
+
 	static constexpr RunningStats calculateFor(const std::vector<StatType>& range)
 	{
 		RunningStats stats;
@@ -41,13 +46,14 @@ public:
 		return stats;
 	}
 
-	void addSample(const StatType& sample)
+	void addSample(StatType sample)
 	{
+		lastSample = std::move(sample);
 		++counter;
-		const StatType delta = sample - mean;
+
+		const auto delta = lastSample - mean;
 		mean += delta / counter;
-		m2 += delta * (sample - mean);
-		lastSample = sample;
+		m2 += delta * (lastSample - mean);
 	}
 
 	StatType getLastSample() const { return lastSample; }
@@ -56,35 +62,103 @@ public:
 
 	StatType getVariance() const { return counter < 2 ? StatType{} : m2 / counter; }
 
-	auto getStdDev() const
+	StatType getStdDev() const { return std::sqrt(getVariance()); }
+
+	auto getMeanAndStdDev() const { return std::make_pair(getMean(), getStdDev()); }
+
+private:
+	StatType lastSample{0};
+	size_t counter{0};
+
+	StatType mean{0};
+	StatType m2{0};
+};
+
+// RunningStats specialization targeting Vector types. This is important because of the details in addSample method and additional methods provided (covariance).)
+template<int dim, typename T>
+class RunningStats<Vector<dim, T>>
+{
+public:
+	using StatType = Vector<dim, T>;
+
+	static constexpr RunningStats calculateFor(const std::vector<StatType>& range)
 	{
-		// This if-statement is only for handling floating point numbers.
-		if constexpr (std::is_floating_point_v<StatType>) {
-			return std::sqrt(getVariance());
+		RunningStats stats;
+		for (auto&& element : range) {
+			stats.addSample(element);
+		}
+		return stats;
+	}
+
+	void addSample(StatType sample)
+	{
+		lastSample = std::move(sample);
+		++counter;
+
+		const auto prevDelta = lastSample - mean;
+		mean += prevDelta / counter;
+		const auto currentDelta = lastSample - mean;
+
+		m2 += prevDelta * currentDelta;
+
+		if constexpr (dim >= 2) {
+			sumCov[0] = sumCov.x() + prevDelta.x() * currentDelta.y();
 		}
 
-		// If-statement below all handle Vector types.
+		if constexpr (dim >= 3) {
+			sumCov[1] = sumCov.y() + prevDelta.y() * currentDelta.z();
+			sumCov[2] = sumCov.z() + prevDelta.z() * currentDelta.x();
+		}
+	}
+
+	StatType getLastSample() const { return lastSample; }
+
+	StatType getMean() const { return mean; }
+
+	StatType getVariance() const { return counter < 2 ? StatType{0} : m2 / counter; }
+
+	template<int subDim = dim, typename = std::enable_if_t<subDim >= 2>>
+	T getCovarianceXY() const
+	{
+		return counter < 2 ? T{0} : sumCov.x() / counter;
+	}
+
+	template<int subDim = dim, typename = std::enable_if_t<subDim >= 3>>
+	T getCovarianceYZ() const
+	{
+		return counter < 2 ? T{0} : sumCov.y() / counter;
+	}
+
+	template<int subDim = dim, typename = std::enable_if_t<subDim >= 3>>
+	T getCovarianceZX() const
+	{
+		return counter < 2 ? T{0} : sumCov.z() / counter;
+	}
+
+	StatType getStdDev() const
+	{
 		const auto variance = getVariance();
 
-		if constexpr (std::is_same_v<StatType, Vector<2, float>>) {
-			return Vector<2, float>{std::sqrt(variance.x()), std::sqrt(variance.y())};
+		if constexpr (dim == 2) {
+			return {std::sqrt(variance.x()), std::sqrt(variance.y())};
 		}
 
-		if constexpr (std::is_same_v<StatType, Vector<3, float>>) {
-			return Vector<3, float>{std::sqrt(variance.x()), std::sqrt(variance.y()), std::sqrt(variance.z())};
+		if constexpr (dim == 3) {
+			return {std::sqrt(variance.x()), std::sqrt(variance.y()), std::sqrt(variance.z())};
 		}
 
-		if constexpr (std::is_same_v<StatType, Vector<4, float>>) {
-			return Vector<4, float>{std::sqrt(variance[0]), std::sqrt(variance[1]), std::sqrt(variance[2]),
-			                        std::sqrt(variance[3])};
+		if constexpr (dim == 4) {
+			return {std::sqrt(variance[0]), std::sqrt(variance[1]), std::sqrt(variance[2]), std::sqrt(variance[3])};
 		}
 	}
 
 	auto getMeanAndStdDev() const { return std::make_pair(getMean(), getStdDev()); }
 
 private:
+	StatType lastSample{0};
 	size_t counter{0};
+
 	StatType mean{0};
 	StatType m2{0};
-	StatType lastSample{0};
+	StatType sumCov{0}; //< Sum for calculating covariance.
 };
