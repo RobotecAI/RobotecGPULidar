5117a3a9e71025f1b465627cad278fe97ecf7c00
Paweł Liberadzki <pawel.liberadzki@robotec.ai>
2024-08-02 13:18:46 +0200

    Make cleanup and minor refactors in nodeKernels.
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>

--- src/gpu/nodeKernels.cu
+++ src/gpu/nodeKernels.cu
@@ -100,7 +100,8 @@ __device__ Vec3f reflectPolarization(const Vec3f& pol, const Vec3f& hitNormal, c
 	return -polCompR * refPolR + polCompU * refPolU;
 }
 
-__device__ void saveReturnHit(const RaytraceRequestContext* ctx, int beamIdx, int sampleIdx, int returnPointIdx, int returnType)
+__device__ void saveReturnAsHit(const RaytraceRequestContext* ctx, int beamIdx, int sampleIdx, int returnPointIdx,
+                                int returnType)
 {
 	if (ctx->xyz != nullptr) {
 		const Mat3x4f ray = ctx->raysWorld[beamIdx];
@@ -152,10 +153,12 @@ __device__ void saveReturnHit(const RaytraceRequestContext* ctx, int beamIdx, in
 	}
 }
 
-__device__ void saveReturnNonHit(const RaytraceRequestContext* ctx, int beamIdx, int returnPointIdx, int returnType)
+__device__ void saveReturnAsNonHit(const RaytraceRequestContext* ctx, int beamIdx, int returnPointIdx, int returnType)
 {
-	// Arbitrary decision - if all samples are non hit, I just take the distance from center ray. This distance will be either
-	// ctx.nearNonHitDistance or ctx.farNonHitDistance, based on optix kernel processing.
+	// Arbitrary decision - if all samples are non hits, I just take the distance from center ray. This distance will be either
+	// ctx.nearNonHitDistance or ctx.farNonHitDistance, based on optix kernel processing - check optixPrograms.cu. This provides
+	// being able to set below minRange, above maxRange and non hit results with the same code. Moreover, results for sample
+	// at idx 0 are always present with current implementation.
 	const auto nonHitDistance = ctx->mrSamples.distance[0];
 
 	if (ctx->xyz != nullptr) {
@@ -183,7 +186,6 @@ __device__ void saveReturnNonHit(const RaytraceRequestContext* ctx, int beamIdx,
 		ctx->intensityF32[returnPointIdx] = 0;
 	}
 	if (ctx->intensityU8 != nullptr) {
-		// intensity < 0 not possible
 		ctx->intensityU8[returnPointIdx] = 0;
 	}
 	if (ctx->entityId != nullptr) {
@@ -343,11 +345,7 @@ __global__ void kReduceDivergentBeams(size_t beamCount, int samplesPerBeam, rgl_
 	int strongest = -1, secondStrongest = -1;
 	int sampleIdx = firstSampleInBeamIdx;
 
-//	printf("firstSampleIdx: %d samplesPerBeam: %d\n", firstSampleInBeamIdx, samplesPerBeam);
-
 	for (; sampleIdx < firstSampleInBeamIdx + samplesPerBeam; ++sampleIdx) {
-		//		printf("sample after optix %f %f %f\n", ctx->mrSamples.xyz[sampleIdx].x(), ctx->mrSamples.xyz[sampleIdx].y(),
-		//		       ctx->mrSamples.xyz[sampleIdx].z());
 		if (ctx->mrSamples.isHit[sampleIdx] == 1) {
 			first = sampleIdx;
 			second = first;
@@ -359,19 +357,16 @@ __global__ void kReduceDivergentBeams(size_t beamCount, int samplesPerBeam, rgl_
 	}
 
 	const auto returnCount = returnMode >> RGL_RETURN_MODE_BIT_SHIFT;
-	//printf("returnMode: %d returnCount: %d\n", returnMode, returnCount);
 
 	// There were no hits within beam samples.
 	if (first == -1) {
 		for (int returnIdx = 0; returnIdx < returnCount; ++returnIdx) {
 			const auto returnPointIdx = beamIdx * returnCount + returnIdx;
 			const auto returnType = (returnMode >> (returnIdx * RGL_RETURN_TYPE_BIT_SHIFT)) & 0xff;
-			saveReturnNonHit(ctx, beamIdx, returnPointIdx, returnType);
+			saveReturnAsNonHit(ctx, beamIdx, returnPointIdx, returnType);
 		}
 		return;
 	}
-
-	//printf("reference sample idx: %d\n", sampleIdx);
 
 	// Base initial values on first sample that returned a hit.
 	for (sampleIdx = sampleIdx + 1; sampleIdx < firstSampleInBeamIdx + samplesPerBeam; ++sampleIdx) {
@@ -379,7 +374,6 @@ __global__ void kReduceDivergentBeams(size_t beamCount, int samplesPerBeam, rgl_
 			continue;
 		}
 
-		// TODO(Pawel): Revise this logic.
 		const auto currentSampleDistance = ctx->mrSamples.distance[sampleIdx];
 		if (currentSampleDistance < ctx->mrSamples.distance[first]) {
 			// Sample is closer than first. I ignore current == first, because then second would become equal to first.
@@ -406,35 +400,25 @@ __global__ void kReduceDivergentBeams(size_t beamCount, int samplesPerBeam, rgl_
 		}
 	}
 
-//		printf("first: %d, second: %d, last: %d, strongest: %d, secondStrongest: %d\n", first, second, last, strongest,
-//		       secondStrongest);
-
 	for (int returnIdx = 0; returnIdx < returnCount; ++returnIdx) {
 		const auto returnType = (returnMode >> (returnIdx * RGL_RETURN_TYPE_BIT_SHIFT)) & 0xff;
-		int rayIdx = -1;
-
-//		printf("return idx: %d test: %d return types: %d\n", returnIdx, (returnMode >> (returnIdx * RGL_RETURN_TYPE_BIT_SHIFT)) & 0xff, returnType);
+		sampleIdx = -1;
 
 		if (returnType == RGL_RETURN_TYPE_FIRST) {
-			rayIdx = first;
+			sampleIdx = first;
 		} else if (returnType == RGL_RETURN_TYPE_SECOND) {
-			rayIdx = second;
+			sampleIdx = second;
 		} else if (returnType == RGL_RETURN_TYPE_LAST) {
-			rayIdx = last;
+			sampleIdx = last;
 		} else if (returnType == RGL_RETURN_TYPE_STRONGEST) {
-			rayIdx = strongest;
+			sampleIdx = strongest;
 		} else if (returnType == RGL_RETURN_TYPE_SECOND_STRONGEST) {
-			rayIdx = secondStrongest;
+			sampleIdx = secondStrongest;
 		}
 
-		if (rayIdx >= 0) {
+		if (sampleIdx >= 0) {
 			const auto returnPointIdx = beamIdx * returnCount + returnIdx;
-//			printf("assigned sample: %f %f %f\n", ctx->mrSamples.xyz[rayIdx].x(), ctx->mrSamples.xyz[rayIdx].y(),
-//			       ctx->mrSamples.xyz[rayIdx].z());
-			saveReturnHit(ctx, beamIdx, rayIdx, returnPointIdx, returnType);
-			//			printf("assigned xyz: %f %f %f\n", ctx->xyz[returnPointIdx].x(), ctx->xyz[returnPointIdx].y(),
-			//			       ctx->xyz[returnPointIdx].z());
-			//printf("%f %f %f\n", ctx->mrSamples.xyz[sampleIdx].x(), ctx->mrSamples.xyz[sampleIdx].y(), ctx->mrSamples.xyz[sampleIdx].z());
+			saveReturnAsHit(ctx, beamIdx, sampleIdx, returnPointIdx, returnType);
 		}
 	}
 }
