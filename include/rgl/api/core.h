97ea7775dd720991320e49304ea6d66f57a5ed17
PiotrMrozik <piter012@vp.pl>
2024-12-16 12:12:00 +0100

    minor formating fixes

--- include/rgl/api/core.h
+++ include/rgl/api/core.h
@@ -442,10 +442,10 @@ typedef enum : int32_t
 	RGL_FIELD_LASER_RETRO_F32,
 
 	/**
-     * Reflectivity of the hit point.
-     * Reflectivity is a scalar value that describes how much light is reflected by the hit point.
-     * Reflectivity is a property of the material of the hit point.
-     */
+	*  Reflectivity of the hit point.
+	 * Reflectivity is a scalar value that describes how much light is reflected by the hit point.
+	 * Reflectivity is a property of the material of the hit point.
+	 */
 	RGL_FIELD_REFLECTIVITY_F32,
 
 	// Dummy fields
