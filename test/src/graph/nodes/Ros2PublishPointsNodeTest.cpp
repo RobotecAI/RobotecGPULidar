ae17a873bc79e290d7a81e0717f6b468367df02f
Mateusz Szczygielski <mateusz.szczygielski@robotec.ai>
2025-05-19 14:06:35 +0200

    Add test for PointCloud2 message publishing

--- test/src/graph/nodes/Ros2PublishPointsNodeTest.cpp
+++ test/src/graph/nodes/Ros2PublishPointsNodeTest.cpp
@@ -5,6 +5,7 @@
 #include <rgl/api/extensions/ros2.h>
 
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
 
 class Ros2PublishPointsNodeTest : public RGLTest
 {};
@@ -82,4 +83,65 @@ TEST_F(Ros2PublishPointsNodeTest, should_throw_invalid_pipeline_when_no_formatte
 	EXPECT_RGL_SUCCESS(rgl_graph_node_add_child(points, ros2pubWithQos));
 
 	EXPECT_RGL_INVALID_PIPELINE(rgl_graph_run(ros2pubWithQos), "requires a formatted point cloud");
+}
+
+TEST_F(Ros2PublishPointsNodeTest, should_receive_sent_data)
+{
+	const auto POINT_COUNT = 5;
+	const auto TOPIC_NAME = "rgl_test_pointcloud2";
+	const auto FRAME_ID = "rgl_test_frame_id";
+	const auto NODE_NAME = "rgl_test_node";
+	const auto WAIT_TIME_SECS = 1;
+	const auto MESSAGE_REPEATS = 3;
+	const std::vector<rgl_field_t> fields{DISTANCE_F32};
+	assert(fields.size() == 1 && fields[0] == DISTANCE_F32); // Test is hardcoded to work with distance field only.
+	TestPointCloud input(fields, POINT_COUNT);
+
+	// Create nodes
+	rgl_node_t inputNode = input.createUsePointsNode(), pointcloud2Node = nullptr, format = nullptr;
+	EXPECT_RGL_SUCCESS(rgl_node_points_format(&format, fields.data(), fields.size()));
+	ASSERT_RGL_SUCCESS(
+	    rgl_node_points_ros2_publish_with_qos(&pointcloud2Node, TOPIC_NAME, FRAME_ID, QOS_POLICY_RELIABILITY_RELIABLE,
+	                                          QOS_POLICY_DURABILITY_SYSTEM_DEFAULT, QOS_POLICY_HISTORY_SYSTEM_DEFAULT, 0));
+
+	// Connect nodes
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(inputNode, format));
+	ASSERT_RGL_SUCCESS(rgl_graph_node_add_child(format, pointcloud2Node));
+
+	// Synchronization primitives
+	std::atomic<int> messageCount = 0;
+
+	// Create ROS2 subscriber to receive PointCloud2 messages
+	auto node = std::make_shared<rclcpp::Node>(NODE_NAME, rclcpp::NodeOptions{});
+	auto qos = rclcpp::QoS(10);
+	qos.reliability(static_cast<rmw_qos_reliability_policy_t>(QOS_POLICY_RELIABILITY_RELIABLE));
+	qos.durability(static_cast<rmw_qos_durability_policy_t>(QOS_POLICY_DURABILITY_SYSTEM_DEFAULT));
+	qos.history(static_cast<rmw_qos_history_policy_t>(QOS_POLICY_HISTORY_SYSTEM_DEFAULT));
+	auto subscriber = node->create_subscription<sensor_msgs::msg::PointCloud2>(
+	    TOPIC_NAME, qos, [&](const sensor_msgs::msg::PointCloud2::ConstSharedPtr msg) {
+		    EXPECT_EQ(msg->height * msg->width, POINT_COUNT);
+		    EXPECT_EQ(msg->header.frame_id, FRAME_ID);
+		    EXPECT_NE(msg->header.stamp.sec + msg->header.stamp.nanosec, 0);
+
+		    for (int i = 0; i < POINT_COUNT; ++i) {
+			    EXPECT_EQ(reinterpret_cast<const float*>(msg->data.data())[i], input.getFieldValue<DISTANCE_F32>(i));
+		    }
+		    ++messageCount;
+	    });
+
+	// Run
+	for (int i = 0; i < MESSAGE_REPEATS; ++i) {
+		ASSERT_RGL_SUCCESS(rgl_graph_run(inputNode));
+	}
+
+	// Wait for messages
+	{
+		auto start = std::chrono::steady_clock::now();
+		do {
+			rclcpp::spin_some(node);
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		} while (messageCount != MESSAGE_REPEATS &&
+		         std::chrono::steady_clock::now() - start < std::chrono::seconds(WAIT_TIME_SECS));
+		ASSERT_EQ(messageCount, MESSAGE_REPEATS);
+	}
 }
